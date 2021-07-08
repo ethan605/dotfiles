92dc02958e17a314c8518b9632f0209e23462c33
Ethan Nguyen <ethan.nguyen@onfido.com>
2021-07-08 17:58:01 +0100

    Add dot_config/nvim/lua/lsp/init.lua Add dot_config/nvim/lua/lsp/sumneko_lua.lua Add dot_config/nvim/lua/lsp/utils.lua

---
+++ dot_config/nvim/lua/lsp/init.lua
@@ -0,0 +1,26 @@
+local nvim_lsp = require("lspconfig")
+local utils = require("lsp.utils")
+
+-- Use a loop to conveniently call `setup` on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = {
+  "bashls",
+  -- "elixirls",
+  "gopls",
+  "graphql",
+  "html",
+  "jsonls",
+  "pyright",
+  "solargraph",
+  "tsserver",
+  "vimls",
+}
+
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = utils.on_attach,
+    flags = utils.default_flags,
+  }
+end
+
+require("lsp.sumneko_lua")
---
+++ dot_config/nvim/lua/lsp/sumneko_lua.lua
@@ -0,0 +1,38 @@
+local utils = require("lsp.utils")
+
+-- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+local sumneko_root_path = "/usr/share/lua-language-server/main.lua"
+local sumneko_binary = "/usr/bin/lua-language-server"
+
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require("lspconfig").sumneko_lua.setup {
+  on_attach = utils.on_attach,
+  flags = utils.default_flags,
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you"re using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {"vim"},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
