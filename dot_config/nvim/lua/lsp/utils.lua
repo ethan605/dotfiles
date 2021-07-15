aef43818443ba3a634a8b18ccb549f823ae65573
Ethan Nguyen <ethan.nguyen@onfido.com>
2021-07-15 17:29:02 +0100

    Update dot_config/nvim/lua/lsp/utils.lua

--- dot_config/nvim/lua/lsp/utils.lua
+++ dot_config/nvim/lua/lsp/utils.lua
@@ -12,32 +12,29 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
 -- after the language server attaches to the current buffer
 ---@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-  -- Enable completion triggered by <c-x><c-o>
-  -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-  -- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
   local default_opts = { noremap = true, silent = true }
+  local function buf_set_keymap(key, func)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", key, func, default_opts)
+  end
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", default_opts)
-  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", default_opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", default_opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", default_opts)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", default_opts)
-  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", default_opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", default_opts)
-  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", default_opts)
-  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", default_opts)
-  buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", default_opts)
-  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", default_opts)
-  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", default_opts)
-  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", default_opts)
-  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", default_opts)
-  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", default_opts)
-  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", default_opts)
-  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", default_opts)
+  buf_set_keymap("gD", ":lua vim.lsp.buf.declaration()<CR>")
+  buf_set_keymap("gd", ":lua vim.lsp.buf.definition()<CR>")
+  buf_set_keymap("gi", ":lua vim.lsp.buf.implementation()<CR>")
+  buf_set_keymap("gr", ":lua vim.lsp.buf.references()<CR>")
+  buf_set_keymap("K", ":lua vim.lsp.buf.hover()<CR>")
+  buf_set_keymap("[d", ":lua vim.lsp.diagnostic.goto_prev()<CR>")
+  buf_set_keymap("]d", ":lua vim.lsp.diagnostic.goto_next()<CR>")
+  buf_set_keymap("<C-k>", ":lua vim.lsp.buf.signature_help()<CR>")
+  buf_set_keymap("<space>D", ":lua vim.lsp.buf.type_definition()<CR>")
+  buf_set_keymap("<space>ca", ":lua vim.lsp.buf.code_action()<CR>")
+  buf_set_keymap("<space>e", ":lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+  buf_set_keymap("<space>f", ":lua vim.lsp.buf.formatting()<CR>")
+  buf_set_keymap("<space>q", ":lua vim.lsp.diagnostic.set_loclist()<CR>")
+  buf_set_keymap("<space>rn", ":lua vim.lsp.buf.rename()<CR>")
+  buf_set_keymap("<space>wa", ":lua vim.lsp.buf.add_workspace_folder()<CR>")
+  buf_set_keymap("<space>wl", ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
+  buf_set_keymap("<space>wr", ":lua vim.lsp.buf.remove_workspace_folder()<CR>")
 end
 
 return {
