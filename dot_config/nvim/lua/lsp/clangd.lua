e265be5bff9ed298a700966f42875b6e492629e4
Ethan Nguyen <ethan.nguyen@onfido.com>
2022-10-10 14:21:41 +0100

    Add dot_config/nvim/lua/lsp/clangd.lua

---
+++ dot_config/nvim/lua/lsp/clangd.lua
@@ -0,0 +1,10 @@
+local utils = require("lsp.utils")
+
+require("lspconfig").clangd.setup {
+  capabilities = utils.capabilities,
+  flags = utils.default_flags,
+  on_attach = utils.on_attach,
+
+  filetypes = {"c", "cpp", "objc", "objcpp", "cuda"}
+}
+
