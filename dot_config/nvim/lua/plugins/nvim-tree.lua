b4921d636f9f8c5650cd8c4f9ce1517176c8378b
Thanh Nguyen <xtnguyen605@gmail.com>
2023-07-07 10:28:50 +0100

    Update .config/nvim/lua/plugins/nvim-tree.lua

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -25,9 +25,9 @@ require("nvim-tree").setup {
       },
     },
   },
-  update_cwd = false,
+  update_cwd = true,
   update_focused_file = {
     enable = true,
-    update_cwd = false,
+    update_cwd = true,
   },
 }
