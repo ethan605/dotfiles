1678914d1de5eedb440bd74f3523e96c5116dd76
Thanh Nguyen <xtnguyen605@gmail.com>
2023-07-13 09:44:25 +0100

    Update .config/nvim/lua/keymaps/bufferline.lua Update .config/nvim/lua/keymaps/init.lua

--- dot_config/nvim/lua/keymaps/bufferline.lua
+++ dot_config/nvim/lua/keymaps/bufferline.lua
@@ -1,18 +1,18 @@
-local default_opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 
 -- Move to previous/next buffer
-vim.api.nvim_set_keymap("n", "g[", ":BufferLineCyclePrev<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "g]", ":BufferLineCycleNext<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "g[", ":BufferLineCyclePrev<CR>", opts)
+vim.api.nvim_set_keymap("n", "g]", ":BufferLineCycleNext<CR>", opts)
 
 -- Re-order to previous/next buffer
-vim.api.nvim_set_keymap("n", "g{", ":BufferLineMovePrev<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "g}", ":BufferLineMoveNext<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "g{", ":BufferLineMovePrev<CR>", opts)
+vim.api.nvim_set_keymap("n", "g}", ":BufferLineMoveNext<CR>", opts)
 
 -- Manipulate buffers
-vim.api.nvim_set_keymap("n", "gc", ":bdelete!<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "gq", ":BufferLinePickClose<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "gs", ":FzfBuffers<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "gc", ":bdelete!<CR>", opts)
+vim.api.nvim_set_keymap("n", "gq", ":BufferLinePickClose<CR>", opts)
+vim.api.nvim_set_keymap("n", "gs", ":FzfBuffers<CR>", opts)
 
 -- Move to previous/next tab
-vim.api.nvim_set_keymap("n", "t[", ":tabprevious<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "t]", ":tabnext<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "t[", ":tabprevious<CR>", opts)
+vim.api.nvim_set_keymap("n", "t]", ":tabnext<CR>", opts)
--- dot_config/nvim/lua/keymaps/init.lua
+++ dot_config/nvim/lua/keymaps/init.lua
@@ -1,33 +1,34 @@
-local default_opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 
 -- Leader as Easymotion prefix
 vim.api.nvim_set_keymap("n", "<Leader>", "<Plug>(easymotion-prefix)", { silent = true })
 
-vim.api.nvim_set_keymap("n", "<C-o>", ":NvimTreeToggle<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<C-o>", ":NvimTreeToggle<CR>", opts)
 
 -- Utilities from fzf.nvim
-vim.api.nvim_set_keymap("n", "<C-f>", ":FzfFiles<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<C-g>", ":FzfGFiles?<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<C-m>", ":FzfCommands<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "<C-p>", ":FzfGFiles<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<C-f>", ":FzfFiles<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-g>", ":FzfGFiles?<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-m>", ":FzfCommands<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-p>", ":FzfGFiles<CR>", opts)
 
 -- Search globally with RipGrep
 vim.api.nvim_set_keymap("n", "<C-s>", ":Rg<Space>", { noremap = true })
 
 -- Leader + Space to hide search highlights
-vim.api.nvim_set_keymap("n", "<Leader><Space>", ":nohlsearch<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<Leader><Space>", ":nohlsearch<CR>", opts)
 
 -- Leader + v to toggle Vista finder
-vim.api.nvim_set_keymap("n", "<Leader>v", ":Vista finder<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<Leader>v", ":Vista finder<CR>", opts)
 -- Leader + Leader + v to toggle Vista finder
-vim.api.nvim_set_keymap("n", "<Leader><Leader>v", ":Vista!!<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "<Leader><Leader>v", ":Vista!!<CR>", opts)
 
-vim.api.nvim_set_keymap("v", "<C-j>", ":m '>+1<CR>gv=gv", default_opts)
-vim.api.nvim_set_keymap("v", "<C-k>", ":m '<-2<CR>gv=gv", default_opts)
+vim.api.nvim_set_keymap("v", "<C-j>", ":m '>+1<CR>gv=gv", opts)
+vim.api.nvim_set_keymap("v", "<C-k>", ":m '<-2<CR>gv=gv", opts)
 
 -- Gitsigns
-vim.api.nvim_set_keymap("n", "[c", "<cmd>Gitsigns prev_hunk<CR>", default_opts)
-vim.api.nvim_set_keymap("n", "]c", "<cmd>Gitsigns next_hunk<CR>", default_opts)
+vim.api.nvim_set_keymap("n", "[c", "<cmd>Gitsigns prev_hunk<CR>", opts)
+vim.api.nvim_set_keymap("n", "]c", "<cmd>Gitsigns next_hunk<CR>", opts)
 
 require("keymaps.bufferline")
 require("keymaps.compe")
+require("keymaps.nvim-dap")
