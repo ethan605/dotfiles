d2877c34285ac6da3f3c4c9ab69dfa5291380fb2
Thanh Nguyen <xtnguyen605@gmail.com>
2023-07-13 09:44:37 +0100

    Add .config/nvim/lua/keymaps/nvim-dap.lua

---
+++ dot_config/nvim/lua/keymaps/nvim-dap.lua
@@ -0,0 +1,23 @@
+local opts = { noremap = true }
+
+vim.keymap.set("n", "<F5>", ":DapContinue<CR>", opts)
+vim.keymap.set("n", "<F10>", ":DapStepOver<CR>", opts)
+vim.keymap.set("n", "<F11>", ":DapStepInto<CR>", opts)
+vim.keymap.set("n", "<F12>", ":DapStepOut<CR>", opts)
+vim.keymap.set("n", "<Leader>b", ":DapToggleBreakpoint<CR>", opts)
+vim.keymap.set("n", "<Leader>B", function() require("dap").set_breakpoint() end, opts)
+vim.keymap.set("n", "<Leader>lp", function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end, opts)
+vim.keymap.set("n", "<Leader>dr", ":DapToggleRepl<CR>", opts)
+vim.keymap.set("n", "<Leader>dl", function() require("dap").run_last() end, opts)
+
+-- nvim-dap-ui widgets
+vim.keymap.set({"n", "v"}, "<Leader>dh", function() require("dap.ui.widgets").hover() end, opts)
+vim.keymap.set({"n", "v"}, "<Leader>dp", function() require("dap.ui.widgets").preview() end, opts)
+vim.keymap.set("n", "<Leader>df", function()
+  local widgets = require("dap.ui.widgets");
+  widgets.centered_float(widgets.frames);
+end, opts)
+vim.keymap.set("n", "<Leader>ds", function()
+  local widgets = require("dap.ui.widgets");
+  widgets.centered_float(widgets.scopes);
+end, opts)
