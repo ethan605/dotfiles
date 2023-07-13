3e7641f29c4013c8115e5f28620c29058eaf2e98
Thanh Nguyen <xtnguyen605@gmail.com>
2023-07-13 09:48:54 +0100

    Update .config/nvim/lua/keymaps/nvim-dap.lua

--- dot_config/nvim/lua/keymaps/nvim-dap.lua
+++ dot_config/nvim/lua/keymaps/nvim-dap.lua
@@ -1,14 +1,13 @@
-local opts = { noremap = true }
+local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<F5>", ":DapContinue<CR>", opts)
 vim.keymap.set("n", "<F10>", ":DapStepOver<CR>", opts)
 vim.keymap.set("n", "<F11>", ":DapStepInto<CR>", opts)
 vim.keymap.set("n", "<F12>", ":DapStepOut<CR>", opts)
-vim.keymap.set("n", "<Leader>b", ":DapToggleBreakpoint<CR>", opts)
-vim.keymap.set("n", "<Leader>B", function() require("dap").set_breakpoint() end, opts)
-vim.keymap.set("n", "<Leader>lp", function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end, opts)
+vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>", opts)
+vim.keymap.set("n", "<Leader>dl", function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end, opts)
 vim.keymap.set("n", "<Leader>dr", ":DapToggleRepl<CR>", opts)
-vim.keymap.set("n", "<Leader>dl", function() require("dap").run_last() end, opts)
+--vim.keymap.set("n", "<Leader>dl", function() require("dap").run_last() end, opts)
 
 -- nvim-dap-ui widgets
 vim.keymap.set({"n", "v"}, "<Leader>dh", function() require("dap.ui.widgets").hover() end, opts)
