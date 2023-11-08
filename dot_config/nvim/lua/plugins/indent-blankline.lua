42965a7e6afa8dd7cd1e4e277e9415c6d73b07fe
Thanh Nguyen <xtnguyen605@gmail.com>
2023-11-08 17:23:30 +0000

    Add .config/nvim/lua/plugins/indent-blankline.lua

---
+++ dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+require("ibl").setup {
+  indent = { char = "·", tab_char = "·" },
+  whitespace = { remove_blankline_trail = true },
+  scope = {
+    char = "│",
+    highlight = { "Function", "Label" },
+    show_exact_scope = true,
+  },
+  exclude = {
+    filetypes = { 'startify' }
+  },
+}
