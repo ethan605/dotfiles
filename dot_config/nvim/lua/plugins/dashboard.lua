1158e860804d6cbe00ef4f59474e00b35b37d7da
Thanh Nguyen <xtnguyen605@gmail.com>
2023-12-01 22:26:45 +0000

    Add .config/nvim/lua/plugins/dashboard.lua

---
+++ dot_config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,19 @@
+require("dashboard").setup({
+  theme = "hyper",
+  hide = {
+    statusline = true,
+    tabline = true,
+    winbar = true,
+  },
+  shortcut_type = "letter",
+  config = {
+    shortcut = {
+      { desc = "󰓦 Update", group = "DiffAdd", action = "PackerSync", key = "u" },
+      { desc = "󰍉 Check", group = "DiffChange", action = "checkhealth", key = "c" },
+      { desc = "󰿅 Quit", group = "DiffDelete", action = "qa", key = "q" },
+    },
+    packages = { enable = true },
+    project = { enable = false },
+    mru = { limit = 10, icon = " ", label = "MRU", cwd_only = true },
+  },
+})
