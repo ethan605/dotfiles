9c71d52447b8544ebaffb26a7e48c900fac13f88
Thanh Nguyen <xtnguyen605@gmail.com>
2023-12-05 17:34:47 +0000

    Update .config/nvim/lua/lsp/clangd.lua

--- dot_config/nvim/lua/lsp/clangd.lua
+++ dot_config/nvim/lua/lsp/clangd.lua
@@ -4,6 +4,4 @@ require("lspconfig").clangd.setup({
   capabilities = utils.capabilities,
   flags = utils.default_flags,
   on_attach = utils.on_attach,
-
-  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
 })
