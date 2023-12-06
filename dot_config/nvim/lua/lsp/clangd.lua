33e4caab1019ff243f88594d09f6d296fa847ee4
Thanh Nguyen <xtnguyen605@gmail.com>
2023-12-06 16:33:38 +0000

    Update .config/nvim/lua/lsp/clangd.lua

--- dot_config/nvim/lua/lsp/clangd.lua
+++ dot_config/nvim/lua/lsp/clangd.lua
@@ -4,4 +4,5 @@ require("lspconfig").clangd.setup({
   capabilities = utils.capabilities,
   flags = utils.default_flags,
   on_attach = utils.on_attach,
+  --cmd = { "/opt/homebrew/opt/llvm/bin/clangd", "--clang-tidy" },
 })
