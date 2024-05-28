1083db5b65c59bdf2186f1d333ec2c0e9ecb542e
Thanh Nguyen <xtnguyen605@gmail.com>
2024-05-28 15:29:36 +0100

    Update .config/nvim/lua/lsp/init.lua

--- dot_config/nvim/lua/lsp/init.lua
+++ dot_config/nvim/lua/lsp/init.lua
@@ -4,37 +4,36 @@ local utils = require("lsp.utils")
 -- Use a loop to conveniently call `setup` on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = {
-  "bashls",
-  "bufls",
-  "cssls",
-  --"denols",
-  "dockerls",
-  "eslint",
-  "gopls",
-  "graphql",
-  "html",
-  "jsonls",
-  "ltex",
-  "pyright",
-  "ruff_lsp",
-  "rust_analyzer",
-  "solargraph",
-  "terraformls",
-  "tsserver",
-  "vimls",
+	"bashls",
+	"bufls",
+	"cssls",
+	"dockerls",
+	"eslint",
+	"gopls",
+	"graphql",
+	"html",
+	"jsonls",
+	"ltex",
+	"pyright",
+	"ruff_lsp",
+	"rust_analyzer",
+	"solargraph",
+	"terraformls",
+	"tsserver",
+	"vimls",
 }
 
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup({
-    capabilities = utils.capabilities,
-    flags = utils.default_flags,
-    on_attach = utils.on_attach,
-  })
+	nvim_lsp[lsp].setup({
+		capabilities = utils.capabilities,
+		flags = utils.default_flags,
+		on_attach = utils.on_attach,
+	})
 end
 
 require("lsp.clangd")
 require("lsp.diagnosticls")
 require("lsp.elixirls")
---require("lsp.jdtls")
-require("lsp.kotlin_language_server")
+-- require("lsp.jdtls")
+-- require("lsp.kotlin_language_server")
 require("lsp.lua_ls")
