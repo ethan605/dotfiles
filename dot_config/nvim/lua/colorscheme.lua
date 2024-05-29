b7f8139d6d7cf3f3afc1752090b6e71c25437a16
Thanh Nguyen <xtnguyen605@gmail.com>
2024-05-29 21:51:57 +0100

    Update .config/nvim/lua/colorscheme.lua Update .config/nvim/lua/plugins/nvim-highlight-colors.lua

--- dot_config/nvim/lua/colorscheme.lua
+++ dot_config/nvim/lua/colorscheme.lua
@@ -5,24 +5,36 @@ vim.cmd([[
 
 vim.opt.termguicolors = true
 
+local snazzy_colors = {
+  gray = "NvimDarkGrey4",
+  black = "#282a36",
+  blue = "#57c7ff",
+  cyan = "#9aedfe",
+  green = "#5af78e",
+  magenta = "#ff6ac1",
+  red = "#ff5c57",
+  white = "#eff0eb",
+  yellow = "#f3f99d",
+}
+
 -- Searches
-vim.api.nvim_set_hl(0, "Search", { bg = "#f3f99d", fg = "#282a36" }) -- yellow & black
-vim.api.nvim_set_hl(0, "CurSearch", { bg = "#9aedfe", fg = "#282a36" }) -- cyan & black
+vim.api.nvim_set_hl(0, "Search", { bg = snazzy_colors.yellow, fg = snazzy_colors.black })
+vim.api.nvim_set_hl(0, "CurSearch", { bg = snazzy_colors.cyan, fg = snazzy_colors.black })
 vim.api.nvim_set_hl(0, "IncSearch", { link = "Search" })
 
 -- Line limit column colors
-vim.api.nvim_set_hl(0, "ColorColumn", { bg = "NvimDarkGrey4" })
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = snazzy_colors.gray })
 
 -- Matched parentheses colors
-vim.api.nvim_set_hl(0, "MatchParen", { bold = true, fg = "#ff6ac1" }) -- magenta
+vim.api.nvim_set_hl(0, "MatchParen", { bold = true, fg = snazzy_colors.magenta })
 
 -- Float windows
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#3a3a3a" })
-vim.api.nvim_set_hl(0, "FzfLuaBorder", { fg = "NvimDarkGrey4" })
+vim.api.nvim_set_hl(0, "FzfLuaBorder", { fg = snazzy_colors.gray })
 
 -- LSP highlights
-vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#ff5c57" }) -- red
-vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = "#57c7ff" }) -- blue
-vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = "#9aedfe" }) -- cyan
-vim.api.nvim_set_hl(0, "DiagnosticOk", { fg = "#5af78e" }) -- green
-vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = "#f3f99d" }) -- yellow
+vim.api.nvim_set_hl(0, "DiagnosticError", { fg = snazzy_colors.red })
+vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = snazzy_colors.blue })
+vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = snazzy_colors.cyan })
+vim.api.nvim_set_hl(0, "DiagnosticOk", { fg = snazzy_colors.green })
+vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = snazzy_colors.yellow })
--- dot_config/nvim/lua/plugins/nvim-highlight-colors.lua
+++ dot_config/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -4,8 +4,35 @@ require("nvim-highlight-colors").setup({
   enable_tailwind = true,
 
   custom_colors = {
-    -- https://github.com/neovim/neovim/blob/release-0.10/src/nvim/highlight_group.c#L2919
+    -- Default Neovim palettes
+    -- https://github.com/neovim/neovim/blob/release-0.10/src/nvim/highlight_group.c#L2909-L2939
+    { label = "NvimDarkBlue", color = "#004c73" },
+    { label = "NvimDarkCyan", color = "#007373" },
+    { label = "NvimDarkGray1", color = "#07080d" },
+    { label = "NvimDarkGray2", color = "#14161b" },
+    { label = "NvimDarkGray3", color = "#2c2e33" },
+    { label = "NvimDarkGray4", color = "#4f5258" },
+    { label = "NvimDarkGreen", color = "#005523" },
     { label = "NvimDarkGrey1", color = "#07080d" },
+    { label = "NvimDarkGrey2", color = "#14161b" },
+    { label = "NvimDarkGrey3", color = "#2c2e33" },
     { label = "NvimDarkGrey4", color = "#4f5258" },
+    { label = "NvimDarkMagenta", color = "#470045" },
+    { label = "NvimDarkRed", color = "#590008" },
+    { label = "NvimDarkYellow", color = "#6b5300" },
+    { label = "NvimLightBlue", color = "#a6dbff" },
+    { label = "NvimLightCyan", color = "#8cf8f7" },
+    { label = "NvimLightGray1", color = "#eef1f8" },
+    { label = "NvimLightGray2", color = "#e0e2ea" },
+    { label = "NvimLightGray3", color = "#c4c6cd" },
+    { label = "NvimLightGray4", color = "#9b9ea4" },
+    { label = "NvimLightGreen", color = "#b3f6c0" },
+    { label = "NvimLightGrey1", color = "#eef1f8" },
+    { label = "NvimLightGrey2", color = "#e0e2ea" },
+    { label = "NvimLightGrey3", color = "#c4c6cd" },
+    { label = "NvimLightGrey4", color = "#9b9ea4" },
+    { label = "NvimLightMagenta", color = "#ffcaff" },
+    { label = "NvimLightRed", color = "#ffc0b9" },
+    { label = "NvimLightYellow", color = "#fce094" },
   },
 })
