e4a714d67724d45d87c8ae434456f1ba154aecca
Thanh Nguyen <xtnguyen605@gmail.com>
2024-05-30 09:51:01 +0100

    Update .config/nvim/lua/colorscheme.lua

--- dot_config/nvim/lua/colorscheme.lua
+++ dot_config/nvim/lua/colorscheme.lua
@@ -38,3 +38,23 @@ vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = snazzy_colors.blue })
 vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = snazzy_colors.cyan })
 vim.api.nvim_set_hl(0, "DiagnosticOk", { fg = snazzy_colors.green })
 vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = snazzy_colors.yellow })
+
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { sp = snazzy_colors.red })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { sp = snazzy_colors.blue })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { sp = snazzy_colors.cyan })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineOk", { sp = snazzy_colors.green })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { sp = snazzy_colors.yellow })
+
+-- Override default Nvim palettes
+vim.api.nvim_set_hl(0, "Added", { fg = snazzy_colors.green })
+vim.api.nvim_set_hl(0, "RedrawDebugComposed", { bg = snazzy_colors.green })
+
+vim.api.nvim_set_hl(0, "Question", { fg = snazzy_colors.cyan })
+vim.api.nvim_set_hl(0, "QuickFixLine", { bg = snazzy_colors.cyan })
+vim.api.nvim_set_hl(0, "Changed", { bg = snazzy_colors.cyan })
+
+vim.api.nvim_set_hl(0, "Removed", { fg = snazzy_colors.red })
+vim.api.nvim_set_hl(0, "DiagnosticDeprecated", { sp = snazzy_colors.red })
+vim.api.nvim_set_hl(0, "RedrawDebugRecompose", { bg = snazzy_colors.red })
+
+vim.api.nvim_set_hl(0, "RedrawDebugClear", { bg = snazzy_colors.yellow })
