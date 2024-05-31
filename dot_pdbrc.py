94f9355d5d8b05d888c3c77a0e9d66649488c028
Thanh Nguyen <xtnguyen605@gmail.com>
2024-05-31 10:17:13 +0100

    Update .pdbrc.py

--- dot_pdbrc.py
+++ dot_pdbrc.py
@@ -1,4 +1,29 @@
+# Ned's .pdbrc (updated)
+# Source: https://kylekizirian.github.io/ned-batchelders-updated-pdbrc.html
+
 import pdb
 
 class Config(pdb.DefaultConfig):
-    sticky_by_default = True
+    pygments_formatter_class = "pygments.formatters.TerminalTrueColorFormatter"
+    pygments_formatter_kwargs = {"style": "solarized-dark"}
+
+from rich import print
+
+# Print a dictionary, sorted. %1 is the dict, %2 is the prefix for the names.
+alias p_ for k in sorted(%1.keys()): print(f"%2{k.ljust(max(len(s) for s in %1.keys()))} = {%1[k]}")
+
+alias p print(%1)
+#alias pp from rich import print; print(%1)
+
+# Print the member variables of a thing.
+alias pi p_ %1.__dict__ %1.
+
+# Print the member variables of self.
+alias ps pi self
+
+# Print the locals.
+alias pl p_ locals() local:
+
+# Next and list, and step and list.
+alias nl n;;l
+alias sl s;;l
