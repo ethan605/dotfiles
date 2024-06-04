d43ec773cae52a4a362675a5da4ad41fabd56789
Thanh Nguyen <xtnguyen605@gmail.com>
2024-06-04 12:59:34 +0100

    Update .config/nvim/lua/plugins/none-ls.lua Update .ideavimrc

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -2,15 +2,13 @@ local none_ls = require("null-ls")
 
 none_ls.setup({
   sources = {
-    -- completions
-    none_ls.builtins.completion.spell,
-
     -- diagnostics
     none_ls.builtins.diagnostics.zsh,
 
     -- formatters
     none_ls.builtins.formatting.pg_format,
     none_ls.builtins.formatting.prettier,
+    none_ls.builtins.formatting.shfmt,
     none_ls.builtins.formatting.stylua.with({
       extra_args = { "--config-path", vim.fn.expand("~/.config/.stylua.toml") },
     }),
