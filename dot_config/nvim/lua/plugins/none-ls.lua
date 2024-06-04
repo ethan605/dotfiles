28ae2b7ac2a06c8f844b19d88dafdd99b1fb4c61
Thanh Nguyen <xtnguyen605@gmail.com>
2024-06-04 10:04:19 +0100

    Update .Brewfile Update .config/nvim/lua/plugins/none-ls.lua

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,7 @@ none_ls.setup({
     none_ls.builtins.diagnostics.zsh,
 
     -- formatters
+    none_ls.builtins.formatting.pg_format,
     none_ls.builtins.formatting.prettier,
     none_ls.builtins.formatting.stylua.with({
       extra_args = { "--config-path", vim.fn.expand("~/.config/.stylua.toml") },
