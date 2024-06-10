2c18098f21d0d381444d12f85bd1e64b62d571e7
Thanh Nguyen <xtnguyen605@gmail.com>
2024-06-10 09:13:35 +0100

    Add .config/nvim/lua/lsp/rust_analyzer.lua

---
+++ dot_config/nvim/lua/lsp/rust_analyzer.lua
@@ -0,0 +1,44 @@
+local utils = require("lsp.utils")
+
+require("lspconfig").rust_analyzer.setup({
+  capabilities = utils.capabilities,
+  flags = utils.default_flags,
+  on_attach = utils.on_attach,
+
+  settings = {
+    ["rust-analyzer"] = {
+      inlayHints = {
+        bindingModeHints = {
+          enable = false,
+        },
+        chainingHints = {
+          enable = true,
+        },
+        closingBraceHints = {
+          enable = true,
+          minLines = 25,
+        },
+        closureReturnTypeHints = {
+          enable = "never",
+        },
+        lifetimeElisionHints = {
+          enable = "never",
+          useParameterNames = false,
+        },
+        maxLength = 25,
+        parameterHints = {
+          enable = true,
+        },
+        reborrowHints = {
+          enable = "never",
+        },
+        renderColons = true,
+        typeHints = {
+          enable = true,
+          hideClosureInitialization = false,
+          hideNamedConstructor = false,
+        },
+      },
+    },
+  },
+})
