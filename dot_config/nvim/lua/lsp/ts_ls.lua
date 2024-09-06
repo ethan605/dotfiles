9fc915096bc0c660cbbd74d7785bd834c92c8840
Thanh Nguyen <xtnguyen605@gmail.com>
2024-09-06 09:01:05 +0100

    Add .config/nvim/lua/lsp/ts_ls.lua

---
+++ dot_config/nvim/lua/lsp/ts_ls.lua
@@ -0,0 +1,34 @@
+local utils = require("lsp.utils")
+
+require("lspconfig").ts_ls.setup({
+  capabilities = utils.capabilities,
+  flags = utils.default_flags,
+  on_attach = utils.on_attach,
+
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+    javascript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  },
+})
