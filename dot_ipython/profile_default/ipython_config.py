db209924bb31085aa1ee845b36bc7e1265cd9b9f
Thanh Nguyen <xtnguyen605@gmail.com>
2024-09-10 11:23:54 +0100

    Update .ipython/profile_default/ipython_config.py

--- dot_ipython/profile_default/ipython_config.py
+++ dot_ipython/profile_default/ipython_config.py
@@ -1,2 +1,5 @@
-c = get_config()  #noqa
-c.TerminalInteractiveShell.editing_mode = 'vi'
+c = get_config()  # type:ignore[name-defined]
+
+c.InteractiveShellApp.extensions = ["rich"]
+c.TerminalIPythonApp.display_banner = True
+c.TerminalInteractiveShell.editing_mode = "vi"
