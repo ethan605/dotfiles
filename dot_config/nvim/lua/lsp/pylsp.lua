82e95cada85cef8cf5c27695f2273e9411f0ab9d
Thanh Nguyen <xtnguyen605@gmail.com>
2025-02-12 11:24:36 +0000

    Add .config/nvim/lua/lsp/pylsp.lua

---
+++ dot_config/nvim/lua/lsp/pylsp.lua
@@ -0,0 +1,24 @@
+local utils = require("lsp.utils")
+
+require("lspconfig").pylsp.setup({
+  capabilities = utils.capabilities,
+  flags = utils.default_flags,
+  on_attach = utils.on_attach,
+
+  settings = {
+    pylsp = {
+      configurationSources = {},
+      plugins = {
+        autopep8 = { enabled = false },
+        flake8 = { enabled = false },
+        mccabe = { enabled = false },
+        pycodestyle = { enabled = false },
+        pyflake = { enabled = false },
+        yapf = { enabled = false },
+
+        rope_autoimport = { enabled = true },
+        rope_completion = { enabled = false },
+      },
+    },
+  },
+})
