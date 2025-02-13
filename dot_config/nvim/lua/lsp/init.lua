a72b656dd99e5120af7397569cfa6e2b784fc1ee
Thanh Nguyen <xtnguyen605@gmail.com>
2025-02-13 11:53:30 +0000

    Update .config/nvim/lua/lsp/init.lua

--- dot_config/nvim/lua/lsp/init.lua
+++ dot_config/nvim/lua/lsp/init.lua
@@ -1,49 +1,222 @@
 local lspconfig = require("lspconfig")
 local utils = require("lsp.utils")
 
--- Use a loop to conveniently call `setup` on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = {
-  "bashls",
-  "docker_compose_language_service",
-  "dockerls",
-  "eslint",
-  "gopls",
-  "html",
-  "jsonls",
-  "ltex",
-  "pyright",
-  "tailwindcss",
-  "terraformls",
-  "yamlls",
+local servers_with_configs = {
+  -- Plug-n-Play LSPs
+  docker_compose_language_service = {},
+  dockerls = {},
+  eslint = {},
+  gopls = {},
+  html = {},
+  jsonls = {},
+  ltex = {},
+  pyright = {},
+  tailwindcss = {},
+  terraformls = {},
+  yamlls = {},
 
-  -- "cssls", -- disabled in favour of tailwindcss
-  -- "denols", -- disabled due to no active usage
-  -- "graphql", -- disabled due to no active usage
-  -- "jdtls", -- disabled until nvim-java is configured properly
-  -- "solargraph", -- disabled due to no active usage
-  -- "vimls", -- disabled due to no active usage
+  -- Custom LSPs
+  bashls = {
+    filetypes = { "zsh", "bash", "sh" },
+  },
+  diagnosticls = {
+    filetypes = { "javascript", "typescript" },
+    init_options = {
+      linters = {
+        eslint = {
+          command = "./node_modules/.bin/eslint",
+          rootPatterns = { ".eslintrc.js", ".eslintrc", ".git" },
+          debounce = 100,
+          args = {
+            "--stdin",
+            "--stdin-filename",
+            "%filepath",
+            "--format",
+            "json",
+          },
+          sourceName = "eslint",
+          parseJson = {
+            errorsRoot = "[0].messages",
+            line = "line",
+            column = "column",
+            endLine = "endLine",
+            endColumn = "endColumn",
+            message = "[eslint] ${message} [${ruleId}]",
+            security = "severity",
+          },
+          securities = {
+            [2] = "error",
+            [1] = "warning",
+          },
+        },
+      },
+      filetypes = {
+        javascript = "eslint",
+        typescript = "eslint",
+      },
+    },
+  },
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+        hint = {
+          enable = true,
+        },
+        runtime = {
+          version = "LuaJIT",
+        },
+        workspace = {
+          library = {
+            vim.env.VIMRUNTIME,
+          },
+        },
+      },
+    },
+  },
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        inlayHints = {
+          bindingModeHints = {
+            enable = false,
+          },
+          chainingHints = {
+            enable = true,
+          },
+          closingBraceHints = {
+            enable = true,
+            minLines = 25,
+          },
+          closureReturnTypeHints = {
+            enable = "never",
+          },
+          lifetimeElisionHints = {
+            enable = "never",
+            useParameterNames = false,
+          },
+          maxLength = 25,
+          parameterHints = {
+            enable = true,
+          },
+          reborrowHints = {
+            enable = "never",
+          },
+          renderColons = true,
+          typeHints = {
+            enable = true,
+            hideClosureInitialization = false,
+            hideNamedConstructor = false,
+          },
+        },
+      },
+    },
+  },
+  ts_ls = {
+    settings = {
+      typescript = {
+        inlayHints = {
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+      javascript = {
+        inlayHints = {
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+    },
+  },
+
+  --[[ Disabled due to no active usage
+  cssls = {},
+  denols = {},
+  elixirls = {}
+  graphql = {},
+  jdtls = {},
+  kotlin_language_server = {},
+  solargraph = {},
+  vimls = {},
+
+  basedpyright = {
+    settings = {
+      basedpyright = {
+        disableOrganizeImports = true,
+        openFilesOnly = true,
+        analysis = {
+          autoImportCompletions = true,
+          autoSearchPaths = true,
+          diagnosticMode = "openFilesOnly",
+          typeCheckingMode = "standard",
+          useLibraryCodeForTypes = true,
+        },
+      },
+    },
+  },
+  clangd = {
+    cmd = { "/opt/homebrew/opt/llvm/bin/clangd", "--clang-tidy" },
+    settings = {
+      clangd = {
+        InlayHints = {
+          Designators = true,
+          Enabled = true,
+          ParameterNames = true,
+          DeducedTypes = true,
+        },
+        fallbackFlags = { "-std=c++20" },
+      },
+    },
+  },
+  pylsp = {
+    settings = {
+      pylsp = {
+        configurationSources = {},
+        plugins = {
+          autopep8 = { enabled = false },
+          flake8 = { enabled = false },
+          mccabe = { enabled = false },
+          pycodestyle = { enabled = false },
+          pyflake = { enabled = false },
+          yapf = { enabled = false },
+
+          rope_autoimport = { enabled = false },
+          rope_completion = { enabled = false },
+        },
+      },
+    },
+  },
+  ]]
 }
 
-for _, server in ipairs(servers) do
-  lspconfig[server].setup({
+for server, configs in pairs(servers_with_configs) do
+  local settings = {
     capabilities = utils.capabilities,
     flags = utils.default_flags,
     on_attach = utils.on_attach,
-  })
+  }
+
+  -- Merge server configs to settings object
+  for k, v in pairs(configs) do
+    settings[k] = v
+  end
+
+  lspconfig[server].setup(settings)
 end
-
-require("lsp.diagnosticls")
-require("lsp.lua_ls")
-require("lsp.rust_analyzer")
-require("lsp.ts_ls")
-
--- disabled due to no active usage
--- require("lsp.basedpyright")
--- require("lsp.clangd")
--- require("lsp.elixirls")
--- require("lsp.kotlin_language_server")
--- require("lsp.pylsp")
 
 vim.diagnostic.config({
   virtual_text = false, -- for lsp_lines
