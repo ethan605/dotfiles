9e4dc6619615dffdbbb1236dcdcee97170ffc493
Thanh Nguyen <xtnguyen605@gmail.com>
2025-03-22 12:34:03 +0000

    Update .config/nvim/lua/plugins/oil.lua

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,42 @@
+local function parse_output(proc)
+  local result = proc:wait()
+  local ret = {}
+  if result.code == 0 then
+    for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+      -- Remove trailing slash
+      line = line:gsub("/$", "")
+      ret[line] = true
+    end
+  end
+  return ret
+end
+
+-- build git status cache
+local function new_git_status()
+  return setmetatable({}, {
+    __index = function(self, key)
+      local ignore_proc = vim.system(
+        { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+        {
+          cwd = key,
+          text = true,
+        }
+      )
+      local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+        cwd = key,
+        text = true,
+      })
+      local ret = {
+        ignored = parse_output(ignore_proc),
+        tracked = parse_output(tracked_proc),
+      }
+
+      rawset(self, key, ret)
+      return ret
+    end,
+  })
+end
+
 function _G.get_oil_winbar()
   local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
   local dir = require("oil").get_current_dir(bufnr)
@@ -12,30 +51,57 @@ end
 
 return {
   "stevearc/oil.nvim",
-  opts = {
-    default_file_explorer = false,
-    columns = { "icon", "permissions", "mtime" },
-    win_options = {
-      signcolumn = "yes:2",
-      winbar = "%!v:lua.get_oil_winbar()",
-    },
-    keymaps = {
-      ["g?"] = { "actions.show_help", mode = "n" },
-      ["<CR>"] = "actions.select",
-      ["go"] = { "actions.select" },
-      ["gp"] = "actions.preview",
-      ["gC"] = { "actions.close", mode = "n" },
-      ["gr"] = "actions.refresh",
-      ["<BS>"] = { "actions.parent", mode = "n" },
-      ["g~"] = { "actions.open_cwd", mode = "n" },
-      ["gS"] = { "actions.change_sort", mode = "n" },
-      ["g."] = { "actions.toggle_hidden", mode = "n" },
-      ["g\\"] = { "actions.toggle_trash", mode = "n" },
-    },
-    use_default_keymaps = false,
-  },
   dependencies = {
     "nvim-tree/nvim-web-devicons",
     "refractalize/oil-git-status.nvim",
   },
+  config = function()
+    local git_status = new_git_status()
+
+    -- Clear git status cache on refresh
+    local refresh = require("oil.actions").refresh
+    local orig_refresh = refresh.callback
+    refresh.callback = function(...)
+      git_status = new_git_status()
+      orig_refresh(...)
+    end
+
+    require("oil").setup({
+      default_file_explorer = false,
+      columns = { "icon", "permissions", "mtime" },
+      view_options = {
+        is_hidden_file = function(name, bufnr)
+          local dir = require("oil").get_current_dir(bufnr)
+          local is_dotfile = vim.startswith(name, ".") and name ~= "../"
+          -- if no local directory (e.g. for ssh connections), just hide dotfiles
+          if not dir then return is_dotfile end
+          -- dotfiles are considered hidden unless tracked
+          if is_dotfile then
+            return not git_status[dir].tracked[name]
+          else
+            -- Check if file is gitignored
+            return git_status[dir].ignored[name]
+          end
+        end,
+      },
+      win_options = {
+        signcolumn = "yes:2",
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["go"] = { "actions.select" },
+        ["gp"] = "actions.preview",
+        ["gC"] = { "actions.close", mode = "n" },
+        ["gr"] = "actions.refresh",
+        ["<BS>"] = { "actions.parent", mode = "n" },
+        ["g~"] = { "actions.open_cwd", mode = "n" },
+        ["gS"] = { "actions.change_sort", mode = "n" },
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
+      use_default_keymaps = false,
+    })
+  end,
 }
