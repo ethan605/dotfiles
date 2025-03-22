9fc1ab6426cdb5666b0ed23efc21b720fd50cfda
Thanh Nguyen <xtnguyen605@gmail.com>
2025-03-22 19:29:16 +0000

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lualine.lua Update .config/nvim/lua/plugins/oil.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,104 +1,103 @@
 return {
-	"hoob3rt/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		require("plugins.lsp-progress"), ---@diagnostic disable-line: different-requires
-		{
-			"SmiteshP/nvim-navic",
-			dependencies = { "neovim/nvim-lspconfig" },
-			opts = {
-				highlight = true,
-				lsp = {
-					auto_attach = true,
-					preference = nil,
-				},
-			},
-		},
-	},
-	config = function()
-		local custom_powerline = require("lualine.themes.powerline")
-		local lualine = require("lualine")
+  "hoob3rt/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    require("plugins.lsp-progress"), ---@diagnostic disable-line: different-requires
+    {
+      "SmiteshP/nvim-navic",
+      dependencies = { "neovim/nvim-lspconfig" },
+      opts = {
+        highlight = true,
+        lsp = {
+          auto_attach = true,
+          preference = nil,
+        },
+      },
+    },
+  },
+  config = function()
+    local custom_powerline = require("lualine.themes.powerline")
+    local lualine = require("lualine")
 
-		---@diagnostic disable-next-line: different-requires
-		local snazzy_colors = require("lua.colorscheme").snazzy_colors
+    ---@diagnostic disable-next-line: different-requires
+    local snazzy_colors = require("lua.colorscheme").snazzy_colors
 
-		local function render_lsp_progress()
-			---@diagnostic disable-next-line: different-requires
-			return require("lsp-progress").progress()
-		end
+    local function render_lsp_progress()
+      ---@diagnostic disable-next-line: different-requires
+      return require("lsp-progress").progress()
+    end
 
-		custom_powerline.normal.c.bg = snazzy_colors.black
-		custom_powerline.inactive.c.bg = snazzy_colors.black
+    custom_powerline.normal.c.bg = snazzy_colors.black
+    custom_powerline.inactive.c.bg = snazzy_colors.black
 
-		lualine.setup({
-			options = {
-				component_separators = "",
-				icons_enabled = true,
-				section_separators = "",
-				theme = custom_powerline,
-				disabled_filetypes = { "NvimTree" },
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = {
-					{ "branch", icon = "" },
-					"diff",
-					{
-						"diagnostics",
-						diagnostics_color = {
-							error = "DiagnosticError",
-							warn = "DiagnosticWarn",
-							info = "DiagnosticInfo",
-							hint = "DiagnosticHint",
-						},
-					},
-				},
-				lualine_c = {
-					"filename",
-					{
-						"navic",
-						fmt = function(text)
-							return string.gsub(text, "%%%*$", "")
-						end,
-					},
-				},
-				lualine_x = {
-					{
-						render_lsp_progress,
-						color = { fg = snazzy_colors.white },
-					},
-					"encoding",
-					"fileformat",
-					"filetype",
-				},
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = { "mode" },
-				lualine_b = {
-					{ "branch", icon = "" },
-					"diff",
-					{ "diagnostics", colored = false },
-				},
-				lualine_c = { "filename" },
-				lualine_x = {
-					render_lsp_progress,
-					"encoding",
-					"fileformat",
-					"filetype",
-				},
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-		})
+    lualine.setup({
+      options = {
+        component_separators = "",
+        icons_enabled = true,
+        section_separators = "",
+        theme = custom_powerline,
+        disabled_filetypes = { "NvimTree" },
+      },
+      extensions = { "oil" },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          { "branch", icon = "" },
+          "diff",
+          {
+            "diagnostics",
+            diagnostics_color = {
+              error = "DiagnosticError",
+              warn = "DiagnosticWarn",
+              info = "DiagnosticInfo",
+              hint = "DiagnosticHint",
+            },
+          },
+        },
+        lualine_c = {
+          "filename",
+          {
+            "navic",
+            fmt = function(text) return string.gsub(text, "%%%*$", "") end,
+          },
+        },
+        lualine_x = {
+          {
+            render_lsp_progress,
+            color = { fg = snazzy_colors.white },
+          },
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          { "branch", icon = "" },
+          "diff",
+          { "diagnostics", colored = false },
+        },
+        lualine_c = { "filename" },
+        lualine_x = {
+          render_lsp_progress,
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
 
-		-- For lsp-progress
-		vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-		vim.api.nvim_create_autocmd("User", {
-			group = "lualine_augroup",
-			pattern = "LspProgressStatusUpdated",
-			callback = lualine.refresh,
-		})
-	end,
+    -- For lsp-progress
+    vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = "lualine_augroup",
+      pattern = "LspProgressStatusUpdated",
+      callback = lualine.refresh,
+    })
+  end,
 }
--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -37,18 +37,6 @@ local function new_git_status()
   })
 end
 
-function _G.get_oil_winbar()
-  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-  local dir = require("oil").get_current_dir(bufnr)
-
-  if dir then
-    return vim.fn.fnamemodify(dir, ":~")
-  else
-    -- If there is no current directory (e.g. over ssh), just show the buffer name
-    return vim.api.nvim_buf_get_name(0)
-  end
-end
-
 return {
   "stevearc/oil.nvim",
   dependencies = {
@@ -86,7 +74,6 @@ return {
       },
       win_options = {
         signcolumn = "yes:2",
-        winbar = "%!v:lua.get_oil_winbar()",
       },
       keymaps = {
         ["g?"] = { "actions.show_help", mode = "n" },
