7c2c1d8b2408b5280a46d81ced29cb6ee8c68ca8
Thanh Nguyen <xtnguyen605@gmail.com>
2025-04-07 13:04:03 +0100

    Update .config/nvim/lua/variables.lua

--- dot_config/nvim/lua/variables.lua
+++ dot_config/nvim/lua/variables.lua
@@ -42,5 +42,5 @@ vim.g.db_ui_disable_mappings_sql = 1
 vim.g.db_ui_disable_progress_bar = 1
 vim.g.db_ui_execute_on_save = 0
 vim.g.db_ui_use_nerd_fonts = 1
-vim.g.db_ui_save_location = vim.fn.expand("~/work/scratch/sql")
+vim.g.db_ui_save_location = vim.fn.expand("~/work/scratch-pad/sql")
 vim.g.db_ui_winwidth = 50
