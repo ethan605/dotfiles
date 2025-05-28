947b5450c5aa214be3759b06ff3d6167110aaaba
Thanh Nguyen <xtnguyen605@gmail.com>
2025-05-28 09:50:30 +0100

    Update .config/nvim/lua/plugins/init.lua Update .config/nvim/lua/plugins/nvim-java.lua

--- dot_config/nvim/lua/plugins/nvim-java.lua
+++ dot_config/nvim/lua/plugins/nvim-java.lua
@@ -2,6 +2,8 @@
 return {
   "nvim-java/nvim-java",
   dependencies = {
+    { "mason-org/mason.nvim",           version = "^1.0.0" },
+    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
     "neovim/nvim-lspconfig",
     "mfussenegger/nvim-dap",
   },
