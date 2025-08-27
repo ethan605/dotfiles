3fb186ce06f0e05e72812a840b463f35efd8b759
Thanh Nguyen <xtnguyen605@gmail.com>
2025-08-27 09:16:34 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/avante.lua

--- dot_config/nvim/lua/plugins/avante.lua
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -5,9 +5,12 @@ return {
   version = false,
   ---@type avante.Config
   opts = {
+    ---@type avante.Mode
+    mode = "agentic",
     ---@type avante.ProviderName
     provider = "claude",
-    mode = "agentic",
+    ---type avante.ProviderName
+    -- auto_suggestions_provider = "claude",
     selector = {
       ---@type avante.SelectorProvider
       provider = "fzf_lua",
@@ -15,8 +18,22 @@ return {
         prompt = "Avante> ",
       },
     },
-    dual_boost = { enabled = false },
+    dual_boost = {
+      enabled = true,
+      first_provider = "claude",
+      second_provider = "gemini",
+      prompt =
+      "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+      timeout = 60000, -- Timeout in milliseconds
+    },
     selection = { enabled = false },
+    behaviour = {
+      auto_apply_diff_after_generation = false,
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_suggestions = false,
+      support_paste_from_clipboard = false,
+    },
     web_search_engine = {
       provider = "google",
       proxy = nil,
@@ -27,15 +44,16 @@ return {
       },
     },
     windows = {
+      ---@type AvantePosition
       position = "right",
       wrap = true,
       width = 40,
       input = {
-        height = 10,
-        prompt_hl_group = "Comment",
+        prefix = "> ",
+        height = 15,
       },
       sidebar_header = { enabled = false },
-      edit = { start_insert = false },
+      edit = { start_insert = true },
       ask = { start_insert = false },
     },
   },
