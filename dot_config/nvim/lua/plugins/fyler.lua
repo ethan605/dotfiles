74896336505d68398b4894e18be470ec38e17fd8
Thanh Nguyen <xtnguyen605@gmail.com>
2025-08-29 15:53:21 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/fyler.lua

--- dot_config/nvim/lua/plugins/fyler.lua
+++ dot_config/nvim/lua/plugins/fyler.lua
@@ -3,46 +3,74 @@ return {
   "A7Lavinraj/fyler.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   branch = "main",
-  ---@type FylerConfig
-  opts = {
-    icon_provider = "nvim-web-devicons",
-    views = {
-      explorer = {
-        close_on_select = true,
-        confirm_simple = false,
-        default_explorer = true,
-        git_status = true,
-        indentscope = {
-          enabled = true,
-          group = "FylerIndentMarker",
-          marker = "│",
+  opts = function()
+    local algos = require("fyler.views.explorer.algos")
+    local store = require("fyler.views.explorer.store")
+
+    ---@type FylerConfig
+    return {
+      icon_provider = "nvim-web-devicons",
+      mappings = {
+        ---@type table<string, function|FylerConfigMappingsExplorer>
+        explorer = {
+          ["q"] = "CloseView",
+          ["<CR>"] = "Select",
+          ["l"] = function(view)
+            local itemid = algos.match_itemid(vim.api.nvim_get_current_line())
+            if not itemid then return end
+
+            local entry = store.get_entry(itemid)
+            if not entry:is_dir() then return end
+
+            view.root:find(itemid):toggle()
+            vim.api.nvim_exec_autocmds("User", { pattern = "RefreshView" })
+          end,
+          ["^"] = "GotoParent",
+          ["."] = "GotoCwd",
         },
-        win = {
-          border = "single",
-          kind_presets = {
-            replace = {},
-          },
-          kind = "replace",
-          buf_opts = {},
-          win_opts = {},
+        confirm = {
+          ["y"] = "Confirm",
+          ["n"] = "Discard",
         },
       },
-      confirm = {
-        win = {
-          border = "single",
-          kind_presets = {
-            float = {
-              height = "0.3rel",
-              width = "0.4rel",
-              top = "0.3rel",
-              left = "0.3rel",
+      views = {
+        explorer = {
+          close_on_select = true,
+          confirm_simple = false,
+          default_explorer = true,
+          git_status = true,
+          indentscope = {
+            enabled = true,
+            group = "FylerIndentMarker",
+            marker = "│",
+          },
+          win = {
+            border = "single",
+            kind_presets = {
+              replace = {},
             },
+            kind = "replace",
+            buf_opts = {},
+            win_opts = {},
           },
-          kind = "float",
-          buf_opts = {},
-          win_opts = {},
+        },
+        confirm = {
+          win = {
+            border = "single",
+            kind_presets = {
+              float = {
+                height = "0.3rel",
+                width = "0.4rel",
+                top = "0.3rel",
+                left = "0.3rel",
+              },
+            },
+            kind = "float",
+            buf_opts = {},
+            win_opts = {},
+          },
         },
       },
-    },
-  },
+    }
+  end,
 }
