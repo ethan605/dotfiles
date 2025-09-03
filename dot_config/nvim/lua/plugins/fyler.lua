44f4960397660423f540b7b62a2e441dbb3700b9
Thanh Nguyen <xtnguyen605@gmail.com>
2025-09-03 13:09:54 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/fyler.lua Update .tool-versions

--- dot_config/nvim/lua/plugins/fyler.lua
+++ dot_config/nvim/lua/plugins/fyler.lua
@@ -3,74 +3,49 @@ return {
   "A7Lavinraj/fyler.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   branch = "main",
-  opts = function()
-    -- local algos = require("fyler.views.explorer.algos")
-    -- local store = require("fyler.views.explorer.store")
-
-    ---@type FylerConfig
-    return {
-      icon_provider = "nvim-web-devicons",
-      mappings = {
-        ---@type table<string, function|FylerConfigMappingsExplorer>
-        explorer = {
-          ["q"] = "CloseView",
-          ["<CR>"] = "Select",
-          -- ["l"] = function(view)
-          --   local itemid = algos.match_itemid(vim.api.nvim_get_current_line())
-          --   if not itemid then return end
-          --
-          --   local entry = store.get_entry(itemid)
-          --   if not entry:is_dir() then return end
-          --
-          --   view.root:find(itemid):toggle()
-          --   vim.api.nvim_exec_autocmds("User", { pattern = "RefreshView" })
-          -- end,
-          ["^"] = "GotoParent",
-          ["."] = "GotoCwd",
-        },
-        confirm = {
-          ["y"] = "Confirm",
-          ["n"] = "Discard",
-        },
+  ---@type FylerConfig
+  opts = {
+    icon_provider = "nvim_web_devicons",
+    hooks = {},
+    mappings = {
+      ["q"] = "CloseView",
+      ["<CR>"] = "Select",
+      ["^"] = "GotoParent",
+      ["."] = "GotoCwd",
+    },
+    close_on_select = true,
+    confirm_simple = false,
+    default_explorer = true,
+    git_status = true,
+    indentscope = {
+      enabled = true,
+      group = "FylerIndentMarker",
+      marker = "│",
+    },
+    track_current_buffer = true,
+    win = {
+      border = "single",
+      kind_presets = {
+        replace = {},
       },
-      views = {
-        explorer = {
-          close_on_select = true,
-          confirm_simple = false,
-          default_explorer = true,
-          git_status = true,
-          indentscope = {
-            enabled = true,
-            group = "FylerIndentMarker",
-            marker = "│",
-          },
-          win = {
-            border = "single",
-            kind_presets = {
-              replace = {},
-            },
-            kind = "replace",
-            buf_opts = {},
-            win_opts = {},
-          },
-        },
-        confirm = {
-          win = {
-            border = "single",
-            kind_presets = {
-              float = {
-                height = "0.3rel",
-                width = "0.4rel",
-                top = "0.3rel",
-                left = "0.3rel",
-              },
-            },
-            kind = "float",
-            buf_opts = {},
-            win_opts = {},
-          },
-        },
+      kind = "replace",
+      buf_opts = {
+        filetype = "fyler",
+        syntax = "fyler",
+        buflisted = false,
+        buftype = "acwrite",
+        expandtab = true,
+        shiftwidth = 2,
       },
-    }
-  end,
+      win_opts = {
+        concealcursor = "nvic",
+        conceallevel = 3,
+        cursorline = true,
+        number = true,
+        relativenumber = true,
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder,FloatTitle:FloatTitle",
+        wrap = false,
+      },
+    },
+  },
 }
