410107fc4fe1bf09700469f8e1e15430251ac761
Thanh Nguyen <xtnguyen605@gmail.com>
2025-11-07 15:44:47 +0000

    Add .config/nvim/lua/lsp/pyrefly.lua

---
+++ dot_config/nvim/lua/lsp/pyrefly.lua
@@ -0,0 +1,13 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "pyrefly", "lsp" },
+  filetypes = { "python" },
+  root_markers = {
+    "pyrefly.toml",
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "requirements.txt",
+    "Pipfile",
+  },
+}
