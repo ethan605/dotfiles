444c2aad305976b5f7c34339b90f465a1a387092
Thanh Nguyen <xtnguyen605@gmail.com>
2025-11-12 17:52:22 +0000

    Update .config/nvim/lua/plugins/init.lua Update .config/nvim/lua/plugins/nvim-treesitter.lua

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -33,27 +33,6 @@ local plugins = {
   "mg979/vim-visual-multi",
   "sindrets/diffview.nvim",
 
-  {
-    "hat0uma/csvview.nvim",
-    ---@type CsvView.Options
-    opts = {
-      parser = { comments = { "#", "//" } },
-      keymaps = {
-        -- Text objects for selecting fields
-        textobject_field_inner = { "if", mode = { "o", "x" } },
-        textobject_field_outer = { "af", mode = { "o", "x" } },
-        -- Excel-like navigation:
-        -- Use <Tab> and <S-Tab> to move horizontally between fields.
-        -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
-        -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
-        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
-        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
-        jump_next_row = { "<Enter>", mode = { "n", "v" } },
-        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
-      },
-    },
-    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
-  },
   { "kylechui/nvim-surround", config = true },
   { "windwp/nvim-autopairs",  config = true, event = "InsertEnter" },
   {
@@ -88,6 +67,7 @@ local plugins = {
   require("plugins.smartcolumn"),
   require("plugins.todo-comments"),
   require("plugins.vim-dadbod-ui"),
+  require("plugins.csvview"),
 
   -- Temp. disabled
   -- require("plugins.avante"),
--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,4 +3,12 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  opts = {
+    auto_install = true,
+    ensure_installed = "all",
+    highlight = { enable = true },
+    ignore_install = { "ipkg" },
+    modules = {},
+    sync_install = false,
+  },
 }
