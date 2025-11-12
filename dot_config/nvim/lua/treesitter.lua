7c2042b2b3ccf2d9525bf92fc80d903d7f3539db
Thanh Nguyen <xtnguyen605@gmail.com>
2025-11-12 17:52:09 +0000

    Remove .config/nvim/lua/treesitter.lua

--- dot_config/nvim/lua/treesitter.lua
+++
@@ -1,8 +0,0 @@
-require("nvim-treesitter.configs").setup({
-  auto_install = true,
-  ensure_installed = "all",
-  highlight = { enable = true },
-  ignore_install = { "ipkg" },
-  modules = {},
-  sync_install = false,
-})
