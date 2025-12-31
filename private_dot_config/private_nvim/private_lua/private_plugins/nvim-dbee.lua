b3b18bab0fe5e99339a40132117134087888b786
Thanh Nguyen <xtnguyen605@gmail.com>
2025-12-31 10:13:13 +0000

    Update .config/nvim/lua/plugins/nvim-dbee.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/nvim-dbee.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/nvim-dbee.lua
@@ -6,15 +6,16 @@ return {
   },
   build = function() require("dbee").install() end,
   config = function()
+    local target = os.getenv("SQL_TARGET") or "postgres"
+
     ---@module "dbee"
     ---@type Config
     local cfg = {
       sources = {
-        require("dbee.sources").EnvSource:new("DBEE_POSTGRES_CONNECTIONS"),
-        require("dbee.sources").EnvSource:new("DBEE_CLICKHOUSE_CONNECTIONS"),
+        require("dbee.sources").EnvSource:new("DBEE_CONNECTIONS"),
       },
       editor = {
-        directory = vim.fn.expand("~/work/scratch-pad/sql"),
+        directory = vim.fn.expand("~/work/scratch-pad/sql/dbee/" .. target),
         mappings = {
           { key = "<Leader>e", mode = "v", action = "run_selection" },
         },
