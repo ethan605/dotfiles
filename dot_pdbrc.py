c98fd5a867664cd58c3a345c3ae091f645b16d6e
Thanh Nguyen <xtnguyen605@gmail.com>
2026-01-05 10:03:36 +0000

    Add .pdbrc Update .pdbrc.py

--- dot_pdbrc.py
+++ dot_pdbrc.py
@@ -1,5 +1,11 @@
-from pdb import DefaultConfig
+from pdb import Color  # type: ignore
+from pdb import DefaultConfig  # type: ignore
+
 
 class Config(DefaultConfig):
-    prompt = '(pdb++) '
+    prompt = "(pdb++) "
+    show_hidden_frames_count = True
     sticky_by_default = True
+
+    line_number_color = Color.blue
+    filename_color = Color.yellow
