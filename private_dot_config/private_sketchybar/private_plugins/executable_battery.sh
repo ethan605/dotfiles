660b313c261dd69e4b7a9654bec62385bfbc811d
Thanh Nguyen <xtnguyen605@gmail.com>
2026-01-09 14:38:46 +0000

    Update .config/sketchybar/sketchybarrc Update .config/sketchybar/plugins/battery.sh Update .config/sketchybar/plugins/clock.sh Update .config/sketchybar/plugins/volume.sh

--- private_dot_config/private_sketchybar/private_plugins/executable_battery.sh
+++ private_dot_config/private_sketchybar/private_plugins/executable_battery.sh
@@ -1,26 +1,40 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
-if [ "$PERCENTAGE" = "" ]; then
-  exit 0
+if [[ -z "$PERCENTAGE" ]]; then
+	exit 0
 fi
 
-case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
-  ;;
-  [6-8][0-9]) ICON=""
-  ;;
-  [3-5][0-9]) ICON=""
-  ;;
-  [1-2][0-9]) ICON=""
-  ;;
-  *) ICON=""
-esac
-
-if [ "$CHARGING" != "" ]; then
-  ICON=""
+if [[ ! -z "$CHARGING" ]]; then
+	case "$PERCENTAGE" in
+	9[8-9] | 100) ICON="󰂅" ;;
+	9[0-7]) ICON="󰂋" ;;
+	8[0-9]) ICON="󰂊" ;;
+	7[0-9]) ICON="󰢞" ;;
+	6[0-9]) ICON="󰂉" ;;
+	5[0-9]) ICON="󰢝" ;;
+	4[0-9]) ICON="󰂈" ;;
+	3[0-9]) ICON="󰂇" ;;
+	2[0-9]) ICON="󰂆" ;;
+	1[0-9]) ICON="󰢜" ;;
+	*) ICON="󰢟" ;;
+	esac
+else
+	case "$PERCENTAGE" in
+	9[8-9] | 100) ICON="󰁹" ;;
+	9[0-7]) ICON="󰂂" ;;
+	8[0-9]) ICON="󰂁" ;;
+	7[0-9]) ICON="󰂀" ;;
+	6[0-9]) ICON="󰂀" ;;
+	5[0-9]) ICON="󰁿" ;;
+	4[0-9]) ICON="󰁽" ;;
+	3[0-9]) ICON="󰁼" ;;
+	2[0-9]) ICON="󰁻" ;;
+	1[0-9]) ICON="󰁺" ;;
+	*) ICON="󰂎" ;;
+	esac
 fi
 
 sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
--- private_dot_config/private_sketchybar/private_plugins/executable_clock.sh
+++ private_dot_config/private_sketchybar/private_plugins/executable_clock.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
+sketchybar --set "$NAME" label="$(date '+%a %d %b %H:%M')"
