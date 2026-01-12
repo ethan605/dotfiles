4f54b9ff5a9ff07601b70283c70ad9f8b328ce95
Thanh Nguyen <xtnguyen605@gmail.com>
2026-01-12 17:17:01 +0000

    Update .config/nvim/lua/lsp/sqls.lua Update .config/smug/be.yml

--- private_dot_config/private_nvim/private_lua/private_lsp/sqls.lua
+++ private_dot_config/private_nvim/private_lua/private_lsp/sqls.lua
@@ -1,14 +1,20 @@
-local function get_config_path()
+local function get_driver()
   local target = os.getenv("SQL_TARGET") or "postgres"
-  return vim.fn.expand("~/.config/sqls/config." .. target .. ".yml")
+  return target == "postgres" and "postgresql" or "clickhouse"
 end
 
 ---@type vim.lsp.Config
 return {
-  cmd = {
-    "sqls",
-    "--config",
-    get_config_path(),
-  },
+  cmd = { "sqls" },
   filetypes = { "sql", "mysql", "plsql" },
+  settings = {
+    sqls = {
+      connections = {
+        {
+          driver = get_driver(),
+          dataSourceName = os.getenv("SQLS_CONNECTION_URI"),
+        },
+      },
+    },
+  },
 }
