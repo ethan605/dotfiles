c448c7898d42a35e260fee47241adfaf582f92aa
Thanh Nguyen <xtnguyen605@gmail.com>
2026-01-13 11:15:27 +0000

    Update .config/nvim/mason-lock.json Update .config/nvim/lua/lsp/sqls.lua

--- private_dot_config/private_nvim/private_lua/private_lsp/sqls.lua
+++ private_dot_config/private_nvim/private_lua/private_lsp/sqls.lua
@@ -1,20 +1,11 @@
-local function get_driver()
+local function get_config_path()
   local target = os.getenv("SQL_TARGET") or "postgres"
-  return target == "postgres" and "postgresql" or "clickhouse"
+  return vim.fn.expand("~/.config/sqls/config." .. target .. ".yml")
 end
+
 
 ---@type vim.lsp.Config
 return {
-  cmd = { "sqls" },
+  cmd = { "sqls", "--config", get_config_path() },
   filetypes = { "sql", "mysql", "plsql" },
-  settings = {
-    sqls = {
-      connections = {
-        {
-          driver = get_driver(),
-          dataSourceName = os.getenv("SQLS_CONNECTION_URI"),
-        },
-      },
-    },
-  },
 }
