4a71736a51ba02020223d0f14f435320d4948c5a
Thanh Nguyen <xtnguyen605@gmail.com>
2026-01-27 11:35:04 +0000

    Update .config/nvim/lua/plugins/leap.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/leap.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/leap.lua
@@ -1,6 +1,6 @@
 ---@type LazyPluginSpec
 return {
-  "ggandor/leap.nvim",
+  "https://codeberg.org/andyg/leap.nvim",
   keys = {
     { "<Leader>s", "<Plug>(leap-forward)",  mode = { "n", "x", "o" }, desc = "Leap forward" },
     { "<Leader>S", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap backward" },
