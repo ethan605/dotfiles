6dee7db128fb13eaf4512b267f93b56f302b13ee
Thanh Nguyen <xtnguyen605@gmail.com>
2026-01-30 09:50:13 +0000

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/init.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/init.lua
@@ -46,12 +46,6 @@ local plugins = {
     "brianhuster/live-preview.nvim",
     dependencies = { "ibhagwan/fzf-lua" },
   },
-  {
-    "esmuellert/codediff.nvim",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    cmd = "CodeDiff",
-    config = true,
-  },
 
   -- Plugins with more complex configs
   require("plugins.bufferline"),
