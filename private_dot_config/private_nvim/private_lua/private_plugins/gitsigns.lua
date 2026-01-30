59f690b13d20ec4d4869d40404936e339e38c6fc
Thanh Nguyen <xtnguyen605@gmail.com>
2026-01-30 13:46:47 +0000

    Update .config/nvim/lua/plugins/gitsigns.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/gitsigns.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/gitsigns.lua
@@ -23,7 +23,7 @@ return {
     current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> • <summary>",
   },
   keys = {
-    { "[c", ":Gitsigns prev_hunk<CR>", desc = "Jump to previous hunk" },
-    { "]c", ":Gitsigns next_hunk<CR>", desc = "Jump to next hunk" },
+    { "[h", ":Gitsigns prev_hunk<CR>", desc = "Jump to previous hunk" },
+    { "]h", ":Gitsigns next_hunk<CR>", desc = "Jump to next hunk" },
   },
 }
