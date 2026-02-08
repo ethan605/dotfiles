e1ecb1ec6b2037688893be7a56ab38e7200e4020
Thanh Nguyen <xtnguyen605@gmail.com>
2026-02-08 09:26:30 +0000

    Update .config/nix-darwin/home.nix

--- private_dot_config/private_nix-darwin/private_home.nix
+++ private_dot_config/private_nix-darwin/private_home.nix
@@ -1,98 +1,11 @@
 { pkgs, ... }:
 
-let
-  base = with pkgs; [
-    bash
-    chezmoi
-    coreutils
-    diffutils
-    findutils
-    gnugrep
-    gnused
-    gnutar
-    mise
-    neovim
-    zsh
-  ];
-
-  devel = with pkgs; [
-    binaryen
-    cppcheck
-    dotenvx
-    ffmpeg
-    git
-    git-lfs
-    jless
-    jq
-    llvm
-    openssh
-    openssl
-    yq
-  ];
-
-  docs = with pkgs; [
-    bat
-    bat-extras.core
-    less
-    man
-    man-db
-  ];
-
-  gpg = with pkgs; [
-    gnupg # gpg and gpg-agent
-    gpgme
-    libassuan
-    pinentry_mac
-  ];
-
-  tools = with pkgs; [
-    _1password-cli
-    aria2
-    axel
-    delta
-    direnv
-    duf
-    eza
-    fd
-    fzf
-    httpie
-    keychain
-    ncdu
-    p7zip
-    parallel
-    qrencode
-    ripgrep
-    smug
-    starship
-    tlrc
-    tmux
-    vifm
-    watch
-    wget
-    zbar
-    zoxide
-  ];
-
-  gui = with pkgs; [
-    alacritty
-  ];
-in
 {
   home.stateVersion = "25.11";
 
-  home.packages = base ++ devel ++ docs ++ gpg ++ tools ++ gui;
-
-  # Password store with extensions (properly wrapped)
-  programs.password-store = {
-    enable = true;
-    package = pkgs.pass.withExtensions (exts: [
-      exts.pass-otp
-      exts.pass-update
-    ]);
-  };
-
-  # Browserpass (native messaging host for browser extension)
-  programs.browserpass.enable = true;
+  home.packages = with pkgs; [
+    alacritty
+  ];
 
   # Let home-manager manage itself
   programs.home-manager.enable = true;
