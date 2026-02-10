db6dae017049049853099c1c916c9e4e9d155b62
Thanh Nguyen <xtnguyen605@gmail.com>
2026-02-10 11:05:25 +0000

    Update .config/nix-darwin/home.nix

--- private_dot_config/private_nix-darwin/private_home.nix
+++ private_dot_config/private_nix-darwin/private_home.nix
@@ -15,6 +15,9 @@
 
       profiles.personal = {
         settings = {
+          "browser.startup.homepage" = "about:blank";
+          "browser.newtab.extensionControlled" = true;
+          "browser.startup.homepage_override.extensionControlled" = true;
           "browser.search.region" = "GB";
           "browser.search.isUS" = false;
           "distribution.searchplugins.defaultLocale" = "en-GB";
