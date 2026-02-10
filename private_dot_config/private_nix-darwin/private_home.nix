ffcd4b52901ccf4a8ea39a18d504fb1aab5e2bf7
Thanh Nguyen <xtnguyen605@gmail.com>
2026-02-10 10:55:05 +0000

    Update .config/nix-darwin/home.nix

--- private_dot_config/private_nix-darwin/private_home.nix
+++ private_dot_config/private_nix-darwin/private_home.nix
@@ -20,16 +20,6 @@
           "distribution.searchplugins.defaultLocale" = "en-GB";
           "general.useragent.locale" = "en-GB";
           "browser.bookmarks.showMobileBookmarks" = true;
-          "browser.newtabpage.pinned" = [
-            {
-              title = "Inbox";
-              url = "https://mail.google.com/mail/u/0/#inbox";
-            }
-            {
-              title = "Calendar";
-              url = "https://calendar.google.com/calendar/u/0/r?pli=1";
-            }
-          ];
         };
       };
     };
