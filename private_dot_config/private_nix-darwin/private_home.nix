a732b5ce08275219fd871cfb151d3d5347902004
Thanh Nguyen <xtnguyen605@gmail.com>
2026-02-10 09:58:10 +0000

    Update .config/nix-darwin/home.nix

--- private_dot_config/private_nix-darwin/private_home.nix
+++ private_dot_config/private_nix-darwin/private_home.nix
@@ -3,16 +3,14 @@
 {
   home.stateVersion = "25.11";
 
-  home.packages = with pkgs; [
-    browserpass
-  ];
-
   programs = {
-    browserpass.enable = true;
     home-manager.enable = true;
 
     firefox = {
       enable = true;
+      nativeMessagingHosts = with pkgs; [
+        browserpass
+      ];
     };
   };
 }
