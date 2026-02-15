4757ddc280bd86f5bb67bf6ff8e8b7ba1749e18a
Thanh Nguyen <xtnguyen605@gmail.com>
2026-02-15 09:22:13 +0000

    Update .config/nvim/lua/plugins/nvim-highlight-colors.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/nvim-highlight-colors.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/nvim-highlight-colors.lua
@@ -4,7 +4,40 @@ return {
   opts = {
     render = "background",
     virtual_symbol_position = "inline",
-    enable_tailwind = true,
+    exclude_filetypes = { "man", "txt" },
+
+    ---Highlight hex colors, e.g. '#FFFFFF'
+    enable_hex = true,
+
+    ---Highlight short hex colors e.g. '#fff'
+    enable_short_hex = true,
+
+    ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
+    enable_rgb = true,
+
+    ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
+    enable_hsl = true,
+
+    ---Highlight ansi colors, e.g '\033[0;34m'
+    enable_ansi = true,
+
+    ---Highlight xterm 256 (8bit) colors, e.g '\033[38;5;118m'
+    enable_xterm256 = true,
+
+    ---Highlight xterm True Color (24bit) colors, e.g '\033[38;2;118;64;90m'
+    enable_xtermTrueColor = true,
+
+    -- Highlight hsl colors without function, e.g. '--foreground: 0 69% 69%;'
+    enable_hsl_without_function = true,
+
+    ---Highlight CSS variables, e.g. 'var(--testing-color)'
+    enable_var_usage = true,
+
+    ---Highlight named colors, e.g. 'green'
+    enable_named_colors = true,
+
+    ---Highlight tailwind colors, e.g. 'bg-blue-500'
+    enable_tailwind = false,
 
     custom_colors = {
       { label = "black",            color = "#282a36" },
