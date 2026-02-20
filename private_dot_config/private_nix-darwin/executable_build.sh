9ec01c19bf879d66045f67c0ddd952ac77090543
Thanh Nguyen <xtnguyen605@gmail.com>
2026-02-20 07:53:09 +0000

    Update .config/nix-darwin/build.sh

--- private_dot_config/private_nix-darwin/executable_build.sh
+++ private_dot_config/private_nix-darwin/executable_build.sh
@@ -1,1 +1,12 @@
-nix build ".#darwinConfigurations.macintosh-$(~/.local/bin/get-serial-number).system"
+#!/usr/bin/env bash
+
+# To install:
+# curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+
+machine_id=$(system_profiler SPHardwareDataType | awk -F': ' '/Serial Number/ { print $2 }')
+hostname="macintosh-$machine_id"
+echo "Hostname: $hostname"
+
+sudo scutil --set LocalHostName "$hostname"
+
+/nix/var/nix/profiles/default/bin/nix build ".#darwinConfigurations.$hostname.system"
