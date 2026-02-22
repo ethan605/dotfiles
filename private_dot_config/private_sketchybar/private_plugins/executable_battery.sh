cd47e608b0d42e662df2e6583b58da3acda499e9
Thanh Nguyen <xtnguyen605@gmail.com>
2026-02-22 09:23:59 +0000

    Update .config/sketchybar/plugins/battery.sh Update .config/sketchybar/plugins/mpd.sh

--- private_dot_config/private_sketchybar/private_plugins/executable_battery.sh
+++ private_dot_config/private_sketchybar/private_plugins/executable_battery.sh
@@ -1,45 +1,54 @@
 #!/usr/bin/env bash
 
 # Caveat: BSD grep needs `-E` while GNU grep needs `-P`
-PERCENTAGE="$(pmset -g batt | grep -Po "\d+%" | cut -d% -f1)"
+PERCENTAGE="$(pmset -g batt | grep -Po "\d+%" 2>/dev/null | cut -d% -f1)"
+
+COLOR_RED=0xffff5c57
+COLOR_GREEN=0xff5af78e
+COLOR_YELLOW=0xfff3f99d
 
 if [[ -z "$PERCENTAGE" ]]; then
-	ICON=''
-	LABEL='󱉞'
+	ICON='󱉞'
+	COLOR=''
+	LABEL=''
 else
-  CHARGING="$(pmset -g batt | grep 'AC Power')"
+	CHARGING="$(pmset -g batt | grep 'AC Power')"
 
 	if [[ ! -z "$CHARGING" ]]; then
 		case "$PERCENTAGE" in
-		9[8-9] | 100) ICON="󰂅" ;;
-		9[0-7]) ICON="󰂋" ;;
-		8[0-9]) ICON="󰂊" ;;
-		7[0-9]) ICON="󰢞" ;;
-		6[0-9]) ICON="󰂉" ;;
-		5[0-9]) ICON="󰢝" ;;
-		4[0-9]) ICON="󰂈" ;;
-		3[0-9]) ICON="󰂇" ;;
-		2[0-9]) ICON="󰂆" ;;
-		1[0-9]) ICON="󰢜" ;;
-		*) ICON="󰢟" ;;
+		9[8-9] | 100) ICON='󰂅' ;;
+		9[0-7]) ICON='󰂋' ;;
+		8[0-9]) ICON='󰂊' ;;
+		7[0-9]) ICON='󰢞' ;;
+		6[0-9]) ICON='󰂉' ;;
+		5[0-9]) ICON='󰢝' ;;
+		4[0-9]) ICON='󰂈' ;;
+		3[0-9]) ICON='󰂇' ;;
+		2[0-9]) ICON='󰂆' ;;
+		1[0-9]) ICON='󰢜' ;;
+		*) ICON='󰢟' ;;
 		esac
+
+		COLOR="$COLOR_GREEN"
 	else
 		case "$PERCENTAGE" in
-		9[8-9] | 100) ICON="󰁹" ;;
-		9[0-7]) ICON="󰂂" ;;
-		8[0-9]) ICON="󰂁" ;;
-		7[0-9]) ICON="󰂀" ;;
-		6[0-9]) ICON="󰂀" ;;
-		5[0-9]) ICON="󰁿" ;;
-		4[0-9]) ICON="󰁽" ;;
-		3[0-9]) ICON="󰁼" ;;
-		2[0-9]) ICON="󰁻" ;;
-		1[0-9]) ICON="󰁺" ;;
-		*) ICON="󰂎" ;;
+		9[8-9] | 100) ICON='󰁹' COLOR="$COLOR_GREEN" ;;
+		9[0-7]) ICON='󰂂' COLOR="$COLOR_GREEN" ;;
+		8[0-9]) ICON='󰂁' COLOR="$COLOR_GREEN" ;;
+		7[0-9]) ICON='󰂀' COLOR="$COLOR_YELLOW" ;;
+		6[0-9]) ICON='󰂀' COLOR="$COLOR_YELLOW" ;;
+		5[0-9]) ICON='󰁿' COLOR="$COLOR_YELLOW" ;;
+		4[0-9]) ICON='󰁽' COLOR="$COLOR_YELLOW" ;;
+		3[0-9]) ICON='󰁼' COLOR="$COLOR_RED" ;;
+		2[0-9]) ICON='󰁻' COLOR="$COLOR_RED" ;;
+		1[0-9]) ICON='󰁺' COLOR="$COLOR_RED" ;;
+		*) ICON='󰂎' COLOR="$COLOR_RED" ;;
 		esac
 	fi
 
-  LABEL="${PERCENTAGE}%"
+	LABEL="${PERCENTAGE}%"
 fi
 
-sketchybar --set "$NAME" icon="$ICON" label="$LABEL"
+sketchybar --set "$NAME" \
+	icon="$ICON" icon.color="$COLOR" \
+	label="$LABEL" label.color="$COLOR"
--- private_dot_config/private_sketchybar/private_plugins/executable_mpd.sh
+++ private_dot_config/private_sketchybar/private_plugins/executable_mpd.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
+COLOR_RED=0xffff5c57
+COLOR_BLUE=0xff57c7ff
+COLOR_YELLOW=0xfff3f99d
+
 if mpc status '%state%' >/dev/null 2>&1; then
-	SONG=$(mpc current -f '%title%')
-	POSITION=$(mpc status '%currenttime%/%totaltime%')
+	SONG="$(mpc current -f '%title%')"
+	POSITION="$(mpc status '%currenttime%/%totaltime%')"
 	LABEL="$SONG  •  $POSITION"
 
 	case "$(mpc status '%state%')" in
-	playing) ICON="󰝚" ;;
-	paused) ICON="" ;;
-	*) ICON="" LABEL="󰝛" ;;
+	playing) ICON='󰝚' COLOR="$COLOR_BLUE" ;;
+	paused) ICON='' COLOR="$COLOR_YELLOW" ;;
+
+	*)
+		ICON='󰝛'
+		COLOR="$COLOR_RED"
+		LABEL=''
+		;;
 	esac
 else
-	ICON=""
-	LABEL="󰝛"
+	ICON='󰝛'
+	COLOR="$COLOR_RED"
+	LABEL=''
 fi
 
-sketchybar --set "$NAME" icon="$ICON" label="$LABEL"
+sketchybar --set "$NAME" icon="$ICON" icon.color="$COLOR" label="$LABEL"
