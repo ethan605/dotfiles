0d77f6dc07755222d25dfaf1cd3cb37a8f548736
Thanh Nguyen <xtnguyen605@gmail.com>
2026-02-23 12:31:09 +0000

    Add .config/nix-darwin/modules/services.nix

---
+++ private_dot_config/private_nix-darwin/private_modules/private_services.nix
@@ -0,0 +1,45 @@
+{ pkgs, home-dir, ... }:
+
+{
+  services.sketchybar.enable = true;
+
+  launchd.user.agents = {
+    aerospace = {
+      serviceConfig = {
+        ProgramArguments = [
+          "/usr/bin/open"
+          "-b"
+          "bobko.aerospace"
+        ];
+        RunAtLoad = true;
+        KeepAlive = false;
+      };
+    };
+
+    mpd = {
+      serviceConfig = {
+        ProgramArguments = [
+          "${pkgs.mpd}/bin/mpd"
+          "--no-daemon"
+          "${home-dir}/.config/mpd/mpd.conf"
+        ];
+        KeepAlive = true;
+        RunAtLoad = true;
+      };
+    };
+
+    mpd-idle = {
+      serviceConfig = {
+        KeepAlive = true;
+        RunAtLoad = true;
+      };
+
+      script = ''
+        while true; do
+          ${pkgs.mpc}/bin/mpc idle
+            ${pkgs.sketchybar}/bin/sketchybar --trigger mpd_idle
+            done
+      '';
+    };
+  };
+}
