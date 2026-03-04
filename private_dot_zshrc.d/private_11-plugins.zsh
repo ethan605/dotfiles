f40bc0422f9f4521a79130f176efd737a72e95db
Thanh Nguyen <xtnguyen605@gmail.com>
2026-03-04 17:39:39 +0000

    Update .zshrc.d/11-plugins.zsh

--- private_dot_zshrc.d/private_11-plugins.zsh
+++ private_dot_zshrc.d/private_11-plugins.zsh
@@ -3,14 +3,13 @@
 # Zimfw
 if [[ ! "$ZIM_HOME/init.zsh" -nt "${ZDOTDIR:-$HOME}/.zimrc" ]]; then
   # Update static initialization script if it does not exist or it's outdated, before sourcing it
-  # shellcheck disable=SC1091
   source "$ZIM_HOME/zimfw.zsh" init -q
 fi
 
-# shellcheck disable=SC1091
 source "$ZIM_HOME/init.zsh"
 
 # System plugins
+source <(devpod completion zsh)
 source <(direnv hook zsh)
 source <(starship init zsh)
 source <(zoxide init zsh)
@@ -19,5 +18,4 @@ source <(zoxide init zsh)
 source <(mise activate zsh)
 source <(gt completion)
 source <(helm completion zsh)
-# shellcheck disable=SC1091
 source "$(mise where gcloud)/completion.zsh.inc"
