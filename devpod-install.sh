c581144e6b2d4be72d6929b8ffbedae88ecf135b
Thanh Nguyen <xtnguyen605@gmail.com>
2026-03-06 13:01:31 +0000

    Update devpod-install.sh

--- devpod-install.sh
+++ devpod-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl -fsLS https://get.chezmoi.io | sh -
-chezmoi init https://github.com/ethan605/dotfiles
+chezmoi init --apply https://github.com/ethan605/dotfiles
 
 sudo apt install -y \
 	bat eza neovim vivid zoxide
