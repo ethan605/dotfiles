54ba070ddf072a2e579db91010e4f5e899bf5fe0
Thanh Nguyen <xtnguyen605@gmail.com>
2026-03-06 13:57:46 +0000

    Update .chezmoiignore Change attributes of dotfiles/install.sh Change attributes of dotfiles/.bashrc

--- private_dotfiles/install.sh
+++ private_dotfiles/install.sh
@@ -6,10 +6,18 @@ if ! command -v chezmoi &>/dev/null; then
 	export PATH="$HOME/.local/bin:$PATH"
 fi
 
+rm -rf "$HOME/dotfiles/.*"
+rm -rf "$HOME/dotfiles/LICENSE"
+rm -rf "$HOME/dotfiles/README"
+rm -rf "$HOME/dotfiles/misc"
+rm -rf "$HOME/dotfiles/private_Library"
+rm -rf "$HOME/dotfiles/private_dot_*"
+
 chezmoi init \
 	--promptString machine_id=devpod \
 	--promptString work_email=thanh.nguyen@neo4j.com \
-	--apply https://github.com/ethan605/dotfiles
+	--apply https://github.com/ethan605/dotfiles \
+  --force
 
 # shellcheck disable=SC2024
 sudo -S apt install -y \
