abb2d85cbe5cc0f1e6d4190670a2e165d3cd4898
Thanh Nguyen <xtnguyen605@gmail.com>
2026-03-07 09:18:14 +0000

    Update .config/sketchybar/plugins/input_source.sh

--- private_dot_config/private_sketchybar/private_plugins/private_executable_input_source.sh
+++ private_dot_config/private_sketchybar/private_plugins/private_executable_input_source.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
+COLOR_RED=0xffff5c57
 COLOR_BLUE=0xff57c7ff
-COLOR_YELLOW=0xfff3f99d
 
 VI_TELEX_ON=$(defaults read com.apple.HIToolbox AppleSelectedInputSources | grep -Po 'com.apple.inputmethod.VietnameseTelex')
 
 if [[ -z $VI_TELEX_ON ]]; then
 	ICON=' 󰬰'
-	COLOR="$COLOR_YELLOW"
+	COLOR="$COLOR_BLUE"
 else
 	ICON=' '
-	COLOR="$COLOR_BLUE"
+	COLOR="$COLOR_RED"
 fi
 
 sketchybar --set "$NAME" icon="$ICON" icon.color="$COLOR"
