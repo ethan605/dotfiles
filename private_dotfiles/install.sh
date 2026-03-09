ec6201c4412ffe73397d502a9a9cb5aba08d1b0f
Thanh Nguyen <xtnguyen605@gmail.com>
2026-03-09 09:57:32 +0000

    Update dotfiles/install.sh

--- private_dotfiles/install.sh
+++ private_dotfiles/install.sh
@@ -3,63 +3,82 @@ set -euo pipefail
 
 export PATH="$HOME/.local/bin:$PATH"
 
-# Remove unrelated files
-rm -rf \
-  ~/dotfiles/.* \
-	~/dotfiles/LICENSE \
-	~/dotfiles/README.md \
-	~/dotfiles/misc \
-	~/dotfiles/private_Library \
-	~/dotfiles/private_dot_*
+__pre-clean-up() {
+	# Remove unrelated files
+	rm -rf \
+		~/dotfiles/.* \
+		~/dotfiles/LICENSE \
+		~/dotfiles/README.md \
+		~/dotfiles/misc \
+		~/dotfiles/private_Library \
+		~/dotfiles/private_dot_*
+}
 
-cat "$HOME/.config/devbox/$DEVPOD_WORKSPACE_UID/ubuntu_pw" | sudo -S apt update
+__install-system-packages() {
+	cat "$HOME/.config/devbox/$DEVPOD_WORKSPACE_UID/ubuntu_pw" | sudo -S apt update
 
-sudo apt upgrade -y &&
-	sudo apt install -y \
-		bat eza fd-find gawk kubecolor \
-		kubectx neovim ripgrep vivid zoxide &&
-	sudo apt autoremove -y &&
-	sudo apt clean -y &&
-	sudo rm -rf /var/lib/apt/lists/*
+	sudo apt upgrade -y &&
+		sudo apt install -y \
+			bat eza fd-find gawk kubecolor \
+			kubectx neovim ripgrep vivid zoxide &&
+		sudo apt autoremove -y &&
+		sudo apt clean -y &&
+		sudo rm -rf /var/lib/apt/lists/*
+}
 
-# === chezmoi ===
-rm -rf ~/.local/share/chezmoi
+__configure-chezmoi() {
+	rm -rf ~/.local/share/chezmoi
 
-sh -c "$(curl -fsLS https://chezmoi.io/getlb)" -- -b ~/.local/bin
+	sh -c "$(curl -fsLS https://chezmoi.io/getlb)" -- -b ~/.local/bin
 
-chezmoi init \
-	--promptString machine_id=devpod \
-	--promptString work_email=thanh.nguyen@neo4j.com \
-	--apply https://github.com/ethan605/dotfiles \
-	--force
+	chezmoi init \
+		--promptString machine_id=devpod \
+		--promptString work_email=thanh.nguyen@neo4j.com \
+		--apply https://github.com/ethan605/dotfiles \
+		--force
+}
 
-# === ble.sh ===
-rm -rf \
-  ./ble.sh \
-  ~/.local/share/ble.sh
+__configure-blesh() {
+	rm -rf \
+		./ble.sh \
+		~/.local/share/ble.sh
 
-git clone --recursive --depth 1 --shallow-submodules \
-  https://github.com/akinomyoga/ble.sh.git
+	git clone --recursive --depth 1 --shallow-submodules \
+		https://github.com/akinomyoga/ble.sh.git
 
-make -C ble.sh install PREFIX=~/.local
+	make -C ble.sh install PREFIX=~/.local
+}
 
-# === fzf ===
-rm -rf ~/.fzf
+__configure-fzf() {
+	rm -rf ~/.fzf
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
-~/.fzf/install \
-	--no-key-bindings \
-	--no-completion \
-	--no-update-rc \
-	--no-zsh \
-	--no-fish
+	~/.fzf/install \
+		--no-key-bindings \
+		--no-completion \
+		--no-update-rc \
+		--no-zsh \
+		--no-fish
 
-rm -rf ~/.local/share/fzf-tab-completion
+	rm -rf ~/.local/share/fzf-tab-completion
 
-git clone \
-  https://github.com/lincheney/fzf-tab-completion \
-  ~/.local/share/fzf-tab-completion
+	git clone \
+		https://github.com/lincheney/fzf-tab-completion \
+		~/.local/share/fzf-tab-completion
+}
 
-# Clean-up
-rm -rf ~/dotfiles/ble.sh
+__post-clean-up() {
+	rm -rf ~/dotfiles/ble.sh
+}
+
+__pre-clean-up
+__install-system-packages
+__configure-chezmoi
+__configure-fzf
+
+if [[ -n "$BASH_VERSION" ]]; then
+	__configure-blesh
+fi
+
+__post-clean-up
