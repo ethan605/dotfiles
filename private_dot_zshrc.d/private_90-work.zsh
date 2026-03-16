74bb37dcd6021aec29aa8d0c8f5dd955ce728973
Thanh Nguyen <xtnguyen605@gmail.com>
2026-03-16 14:50:16 +0000

    Update .zshrc.d/90-work.zsh

--- private_dot_zshrc.d/private_90-work.zsh
+++ private_dot_zshrc.d/private_90-work.zsh
@@ -6,7 +6,7 @@ if [[ -f "$WORK_DIR/.zshrc" ]]; then
   source "$WORK_DIR/.zshrc"
 fi
 
-alias devpod-ssh='LC_ALL=C.UTF-8 TERM=xterm-256color ssh neo4j-cloud.devpod'
+alias devpod-ssh='LANG=C.UTF-8 LC_ALL=C.UTF-8 LC_CTYPE=C.UTF-8 TERM=xterm-256color ssh neo4j-cloud.devpod'
 alias wpass='PASSWORD_STORE_DIR="$WORK_DIR/.password-store" pass'
 
 alias psql-local='psql $(wpass postgres/uri-local)'
