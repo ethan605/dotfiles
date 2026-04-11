1ad2c4e5c309e7de07bc97baedafb142bd9f4e98
Thanh Nguyen <xtnguyen605@gmail.com>
2026-04-11 16:20:03 +0100

    Update .config/nvim/lua/plugins/colorscheme.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/private_colorscheme.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_colorscheme.lua
@@ -1,5 +1,15 @@
 ---@type LazyPluginSpec
 return {
   "connorholyday/vim-snazzy",
+  priority = 1000,
   init = function() vim.cmd.colorscheme("snazzy") end,
 }
+
+--@type LazyPluginSpec
+-- return {
+--   "RRethy/nvim-base16",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme("base16-snazzy")
+--   end,
+-- }
