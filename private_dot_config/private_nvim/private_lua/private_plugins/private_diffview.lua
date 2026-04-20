cc937695c7c69a50b90f6564fc060e03336a856e
Thanh Nguyen <xtnguyen605@gmail.com>
2026-04-20 14:25:40 +0100

    Update .config/nvim/lua/plugins/diffview.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/private_diffview.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_diffview.lua
@@ -1,15 +1,31 @@
 ---@type LazyPluginSpec
 return {
   "dlyongemallo/diffview.nvim",
-  ---@type DiffViewOptions
-  opts = {
-    enhanced_diff_hl = true,
-    diffopt = { algorithm = "histogram" },
-    default_args = {
-      DiffviewOpen = { "--imply-local" },
-    },
-    file_panel = {
-      show_branch_name = true,
-    },
-  },
+  opts = function()
+    local actions = require("diffview.actions")
+
+    ---@type DiffViewOptions
+    return {
+      enhanced_diff_hl = true,
+      diffopt = { algorithm = "histogram" },
+      default_args = {
+        DiffviewOpen = { "--imply-local" },
+      },
+      file_panel = {
+        show_branch_name = true,
+        always_show_sections = true,
+      },
+      view = {
+        cycle_layouts = {
+          default = { "diff2_horizontal", "diff1_inline" },
+          merge_tool = { "diff3_horizontal", "diff3_mixed", "diff4_mixed" },
+        },
+      },
+      keymaps = {
+        view = {
+          { "n", "]l", actions.cycle_layout, { desc = "Cycle through available layouts." } },
+        },
+      },
+    }
+  end,
 }
