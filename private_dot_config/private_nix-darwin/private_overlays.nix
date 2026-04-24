406a2779c2c37e7e6beaea1b6626026ac8a3d35a
Thanh Nguyen <xtnguyen605@gmail.com>
2026-04-24 15:25:37 +0100

    Update .config/nix-darwin/flake.lock Update .config/nix-darwin/overlays.nix

--- private_dot_config/private_nix-darwin/private_overlays.nix
+++ private_dot_config/private_nix-darwin/private_overlays.nix
@@ -5,15 +5,15 @@
 }:
 let
   pkgs = (import inputs.nixpkgs { system = system; });
-  pkgs-2511 = (import inputs.nixpkgs-2511 { system = system; });
+  pkgs-25-11 = (import inputs.nixpkgs-25-11 { system = system; });
   pkgs-direnv = (import inputs.nixpkgs-direnv { system = system; });
 in
 {
   nixpkgs = {
     overlays = [
       (_: _: {
-        mpd = pkgs-2511.mpd;
-        zsh = pkgs-2511.zsh;
+        mpd = pkgs-25-11.mpd;
+        zsh = pkgs-25-11.zsh;
         direnv = pkgs-direnv.direnv;
 
         minimal-functional-fox = pkgs.callPackage ./modules/packages/minimal-functional-fox.nix { };
