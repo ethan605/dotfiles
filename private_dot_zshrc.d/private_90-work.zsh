b51e1e6951b1b0cfd2558c6c5ec927466ac8bc94
Thanh Nguyen <xtnguyen605@gmail.com>
2026-06-03 17:01:23 +0100

    Update .zshrc.d/90-work.zsh

--- private_dot_zshrc.d/private_90-work.zsh
+++ private_dot_zshrc.d/private_90-work.zsh
@@ -51,19 +51,42 @@ dbee-postgres() {
   nvim +Dbee
 }
 
-devpod-ssh() {
+devbox() {
+  local for_oc=false oc_port=45678
+
+  while (("$#")); do
+    case "$1" in
+    --for-oc)
+      for_oc=true
+      shift
+      ;;
+    *)
+      shift
+      ;;
+    esac
+  done
+
   LANG=C.UTF-8 LC_ALL=C.UTF-8 LC_CTYPE=C.UTF-8 LC_COLLATE=C.UTF-8
   TERM=xterm-256color
-  OC_PORT=45678
-  ssh -o "SetEnv \
-    OC_PORT=$OC_PORT \
-    OC_GOOGLE_DOCS_MCP_CLIENT_ID=$(wpass api-keys/google-docs-mcp | rg 'client_id:' | awk '{ print $2 }') \
-    OC_GOOGLE_DOCS_MCP_CLIENT_SECRET=$(wpass api-keys/google-docs-mcp | head -1) \
-    OC_GRAFANA_URL=https://grafana-deviam.neo4j-dev.io/ \
-    OC_GRAFANA_SERVICE_ACCOUNT_TOKEN=$(wpass api-keys/grafana-deviam) \
-  " \
-    -L "$OC_PORT::$OC_PORT" \
-    neo4j-cloud.devpod
+
+  if [[ "$for_oc" == "true" ]]; then
+    if lsof -Pi ":$oc_port" -sTCP:LISTEN -t >/dev/null; then
+      echo "Port $oc_port is in use"
+      return 1
+    fi
+
+    ssh -o "SetEnv \
+      OC_PORT=$oc_port \
+      OC_GOOGLE_DOCS_MCP_CLIENT_ID=$(wpass api-keys/google-docs-mcp | rg 'client_id:' | awk '{ print $2 }') \
+      OC_GOOGLE_DOCS_MCP_CLIENT_SECRET=$(wpass api-keys/google-docs-mcp | head -1) \
+      OC_GRAFANA_URL=https://grafana-deviam.neo4j-dev.io/ \
+      OC_GRAFANA_SERVICE_ACCOUNT_TOKEN=$(wpass api-keys/grafana-deviam) \
+      " \
+      -L "$oc_port::$oc_port" \
+      neo4j-cloud.devpod
+  else
+    ssh neo4j-cloud.devpod
+  fi
 }
 
 alias ocattach='opencode attach http://127.0.0.1:45678'
