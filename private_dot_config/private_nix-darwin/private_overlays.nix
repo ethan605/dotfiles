5235df98806fcf1854e676d86829d5ec22b543aa
Thanh Nguyen <xtnguyen605@gmail.com>
2026-07-17 08:56:54 +0100

    Update .config/nix-darwin/overlays.nix

--- private_dot_config/private_nix-darwin/private_overlays.nix
+++ private_dot_config/private_nix-darwin/private_overlays.nix
@@ -7,10 +7,7 @@ in
   nixpkgs.overlays = [
     (_final: prev: {
       # Overriden packages (due to instabilities, bugs, etc.)
-      blueutil = pkgs-26-05.blueutil;
-      sketchybar = pkgs-26-05.sketchybar;
       unar = pkgs-26-05.unar;
-      # zsh = pkgs-26-05.zsh;
 
       # Self-host packages
       minimal-functional-fox = prev.callPackage ./modules/packages/minimal-functional-fox.nix { };
