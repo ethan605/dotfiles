b2f4705f1486c7e691ebb0eda73abed762762947
Thanh Nguyen <xtnguyen605@gmail.com>
2026-07-22 09:01:58 +0100

    Update .config/mise/config.toml Update .config/nix-darwin/overlays.nix Update .config/opencode/opencode.json

--- private_dot_config/private_nix-darwin/private_overlays.nix
+++ private_dot_config/private_nix-darwin/private_overlays.nix
@@ -7,7 +7,7 @@ in
   nixpkgs.overlays = [
     (_final: prev: {
       # Overriden packages (due to instabilities, bugs, etc.)
-      # unar = pkgs-26-05.unar;
+      unar = pkgs-26-05.unar;
 
       # Self-host packages
       minimal-functional-fox = prev.callPackage ./modules/packages/minimal-functional-fox.nix { };
