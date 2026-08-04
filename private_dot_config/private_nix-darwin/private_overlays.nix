068fcf2a6577a2b484b6c398d34a3b9d0cc43d37
Thanh Nguyen <xtnguyen605@gmail.com>
2026-08-04 08:42:19 +0100

    Update .config/mise/config.toml Update .config/nix-darwin/flake.lock Update .config/nix-darwin/overlays.nix Update .config/nvim/lazy-lock.json Update .config/nvim/mason-lock.json

--- private_dot_config/private_nix-darwin/private_overlays.nix
+++ private_dot_config/private_nix-darwin/private_overlays.nix
@@ -1,13 +1,13 @@
 { inputs, system, ... }:
 
 let
-  pkgs-26-05 = import inputs.nixpkgs-26-05 { inherit system; };
+  _pkgs-26-05 = import inputs.nixpkgs-26-05 { inherit system; };
 in
 {
   nixpkgs.overlays = [
     (_final: prev: {
       # Overriden packages (due to instabilities, bugs, etc.)
-      unar = pkgs-26-05.unar;
+      # unar = pkgs-26-05.unar;
 
       # Self-host packages
       minimal-functional-fox = prev.callPackage ./modules/packages/minimal-functional-fox.nix { };
