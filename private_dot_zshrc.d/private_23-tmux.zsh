0c8dfa514f0938bea7a4868f44ad15b867865bf0
Thanh Nguyen <xtnguyen605@gmail.com>
2026-08-11 16:48:56 +0100

    Update .zshrc.d/23-tmux.zsh

--- private_dot_zshrc.d/private_23-tmux.zsh
+++ private_dot_zshrc.d/private_23-tmux.zsh
@@ -3,10 +3,27 @@ alias matrix='neo --async --bold=2 --defaultbg --fullwidth --screensaver --speed
 alias pipes='pipes-rs --fps=60 --kinds=light,curved,knobby --pipe-num=$(($(od -vAn -N1 -tu1 < /dev/urandom) % 10))'
 
 greek-letters() {
-  print -P "alpha: %F{red}α%f,beta: %F{red}β%f,gamma: %F{red}γ%f,delta: %F{red}δ%f,epsilon: %F{red}ε%f,theta: %F{red}θ%f,lambda: %F{red}λ%f,mu: %F{red}μ%f,pi: %F{red}π%f,sigma: %F{red}σ%f,phi: %F{red}φ%f,omega: %F{red}ω%f" \
-    | tr ',' '\n' \
-    | fzf --ansi \
-    | awk '{ print $2 }'
+  local items=$(print -P "
+    alpha: %F{red}α%f
+    beta: %F{red}β%f
+    gamma: %F{red}γ%f
+    delta: %F{red}δ%f
+    epsilon: %F{red}ε%f
+    theta: %F{red}θ%f
+    lambda: %F{red}λ%f
+    mu: %F{red}μ%f
+    pi: %F{red}π%f
+    sigma: %F{red}σ%f
+    phi: %F{red}φ%f
+    omega: %F{red}ω%f
+  ")
+
+  echo "$items" |
+    awk 'NF' |            # remove blank lines
+    sed 's/^\s*//' |      # remove leading spaces
+    fzf --ansi |          # preserve colors
+    awk '{ print $2 }' |  # select symbols
+    tr -d '\n'            # remove trailing newline
 }
 
 mux() {
