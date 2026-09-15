29d585e5a83d2975734ca9518631383e4925a539
Thanh Nguyen <xtnguyen605@gmail.com>
2026-09-15 14:46:08 +0100

    Add .config/nvim/lua/plugins/blink.lua

---
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_blink.lua
@@ -0,0 +1,54 @@
+---@type LazyPluginSpec
+return {
+  "saghen/blink.cmp",
+  version = "1.*",
+
+  ---@module "blink.cmp"
+  ---@type blink.cmp.Config
+  opts = {
+    appearance = { nerd_font_variant = "mono" },
+
+    sources = {
+      default = { "lsp", "snippets", "buffer" },
+      providers = {
+        lsp = {
+          fallbacks = {},
+        },
+      },
+    },
+
+    snippets = { preset = "default" },
+
+    keymap = {
+      preset = "default",
+      ["<C-e>"] = { "cancel", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+    },
+
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      menu = {
+        draw = {
+          columns = {
+            { "label",     "label_description", gap = 1 },
+            { "kind_icon", "kind" },
+          },
+        },
+      },
+    },
+
+    cmdline = {
+      keymap = {
+        preset = "inherit",
+        ["<CR>"] = { "accept_and_enter", "fallback" },
+      },
+      completion = { menu = { auto_show = true } },
+      sources = { "path", "cmdline", "buffer" },
+    },
+  },
+}
