981e22360e40a912d906332ea36ef451e81b5980
Thanh Nguyen <xtnguyen605@gmail.com>
2026-09-16 15:19:09 +0100

    Update .config/nvim/lua/plugins/blink.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/private_blink.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_blink.lua
@@ -52,7 +52,7 @@ return {
     cmdline = {
       keymap = {
         ["<Tab>"] = { "show", "select_next" },
-        ["<CR>"] = { "accept_and_enter", "fallback" },
+        ["<CR>"] = { "select_and_accept", "fallback" },
       },
       completion = { menu = { auto_show = true } },
     },
