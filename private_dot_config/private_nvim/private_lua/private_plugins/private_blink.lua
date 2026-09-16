ef67300fb9fe230cea0479b235f3064aeb446699
Thanh Nguyen <xtnguyen605@gmail.com>
2026-09-16 11:23:10 +0100

    Update .config/nvim/lua/plugins/blink.lua

--- private_dot_config/private_nvim/private_lua/private_plugins/private_blink.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_blink.lua
@@ -11,6 +11,13 @@ return {
     sources = {
       default = { "lsp", "snippets", "buffer" },
       providers = {
+        cmdline = {
+          min_keyword_length = function(ctx)
+            -- Disable completions for single chars (e.g. :w)
+            if ctx.mode == "cmdline" and string.find(ctx.line, " ") == nil then return 2 end
+            return 0
+          end,
+        },
         lsp = {
           fallbacks = {},
         },
@@ -22,7 +29,7 @@ return {
     keymap = {
       preset = "default",
       ["<C-e>"] = { "cancel", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
+      ["<CR>"] = { "select_and_accept", "fallback" },
       ["<Tab>"] = { "select_next", "fallback" },
       ["<S-Tab>"] = { "select_prev", "fallback" },
     },
@@ -44,11 +51,10 @@ return {
 
     cmdline = {
       keymap = {
-        preset = "inherit",
-        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "show", "select_next" },
+        ["<CR>"] = { "accept_and_enter", "fallback" },
       },
       completion = { menu = { auto_show = true } },
-      sources = { "path", "cmdline", "buffer" },
     },
   },
 }
