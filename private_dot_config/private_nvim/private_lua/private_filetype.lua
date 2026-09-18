87627bfbceb741c8b7354777c4918e690bc5d5bf
Thanh Nguyen <xtnguyen605@gmail.com>
2026-09-18 12:00:31 +0100

    Update .config/nvim/lua/filetype.lua

--- private_dot_config/private_nvim/private_lua/private_filetype.lua
+++ private_dot_config/private_nvim/private_lua/private_filetype.lua
@@ -1,5 +1,6 @@
 vim.filetype.add({
   filename = {
+    [".envrc"] = "zsh",
     [".zhistory"] = "zsh",
     ["docker-compose.yml"] = "yaml.docker-compose",
     ["docker-compose.yaml"] = "yaml.docker-compose",
